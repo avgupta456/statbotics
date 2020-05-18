abe17c67e80173a6402a07c72437f517be3650e1
Abhijit Gupta <avgupta456@gmail.com>
2020-05-18 00:34:05 -0400

    Update settings.py

--- backend/settings.py
+++ backend/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import constants
+from . import constants
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
