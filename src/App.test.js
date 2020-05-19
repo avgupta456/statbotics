82c89304c4d33631a40e950d3f3089dd35213fd0
Abhijit Gupta <avgupta456@gmail.com>
2020-05-18 21:16:24 -0400

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -4,6 +4,6 @@ import App from './App';
 
 test('renders Hello World', () => {
   const { getByText } = render(<App />);
-  const linkElement = getByText(/Hello World/i);
+  const linkElement = getByText(/Teams/i);
   expect(linkElement).toBeInTheDocument();
 });
