7d500532cdfadb4083ff977ee16b6e5da21341e5
Abhijit Gupta <avgupta456@gmail.com>
2020-05-19 19:55:30 -0400

    bug fixes, decimal rounding

--- prep_data.py
+++ prep_data.py
@@ -3,6 +3,7 @@ import pandas as pd
 import stats
 
 import statistics
+import json
 
 def get_data(start_year, end_year):
     team_matches = []
@@ -12,12 +13,12 @@ def get_data(start_year, end_year):
             match = m.key.split("_")[1]
 
             for i in range(len(m.red)):
-                start_elo, end_elo = m.red_ratings[i], m.red_ratings_end[i]
+                start_elo, end_elo = round(m.red_ratings[i], 2), round(m.red_ratings_end[i], 2)
                 elo_diff = end_elo - start_elo
                 team_matches.append([year, event, match, m.red[i], start_elo, end_elo, elo_diff])
 
             for i in range(len(m.blue)):
-                start_elo, end_elo = m.blue_ratings[i], m.blue_ratings_end[i]
+                start_elo, end_elo = round(m.blue_ratings[i], 2), round(m.blue_ratings_end[i], 2)
                 elo_diff = end_elo - start_elo
                 team_matches.append([year, event, match, m.blue[i], start_elo, end_elo, elo_diff])
 
@@ -39,7 +40,7 @@ def get_data(start_year, end_year):
                     elo_pre_playoffs = temp.iloc[temp.shape[0]-1,5]
 
                 elo_end = team_data.iloc[team_data.shape[0]-1, 5]
-                elo_mean = team_data['elo_end'].mean()
+                elo_mean = round(team_data['elo_end'].mean(), 2)
                 elo_max = team_data['elo_end'].max()
                 elo_diff = elo_end - elo_start
 
@@ -55,11 +56,11 @@ def get_data(start_year, end_year):
         year_data_events = team_events[team_events.year == year]
         year_data_matches = team_matches[team_matches.year == year]
 
-        teams = utils.loadTeams(year)
-        num_teams = len(teams)
+        teams_temp = utils.loadTeams(year)
+        num_teams = len(teams_temp)
 
         ratings = []
-        for t in teams.values():
+        for t in teams_temp.values():
             ratings.append(t.get_rating_max())
         ratings.sort()
 
@@ -73,11 +74,11 @@ def get_data(start_year, end_year):
             if(len(set(champ_keys).intersection(set(team_data_events["event"])))==0): elo_pre_champs = elo_end
             else: elo_pre_champs = team_data_events[team_data_events.event.isin(champ_keys)].iloc[0, 3]
 
-            elo_mean = team_data_matches['elo_end'].mean()
-            elo_max = teams[team].get_rating_max()
+            elo_mean = round(team_data_matches['elo_end'].mean(), 2)
+            elo_max = teams_temp[team].get_rating_max()
 
             rank = num_teams-ratings.index(elo_max)
-            percent = int(1e4*rank/num_teams)/1e2
+            percent = round((rank/num_teams), 2)
 
             team_years.append([year, team, elo_start, elo_pre_champs, elo_end, elo_mean, elo_max, elo_diff, rank, percent])
 
@@ -90,7 +91,7 @@ def get_data(start_year, end_year):
         for i in range(team_data.shape[0]):
             elos[team_data["year"].iloc[i]-start_year]=team_data["elo_max"].iloc[i]
             elo_sum, count = elo_sum + team_data["elo_max"].iloc[i], count + 1
-        elo, elo_mean, elo_max = elos[-1], elo_sum/count, max(elos)
+        elo, elo_mean, elo_max = elos[-1], round(elo_sum/count, 2), max(elos)
         elo_max_year = start_year+elos.index(elo_max)
         elos = ", ".join(str(x) for x in elos)
         teams.append([team, elo, elos, elo_mean, elo_max, elo_max_year])
@@ -105,6 +106,7 @@ def get_data(start_year, end_year):
             event_data = year_data[year_data.event == event]
             for i in range(event_data.shape[0]):
                 elos.append(event_data["elo_max"].iloc[i])
+            elos.sort(reverse=True)
 
             try: elo_max = elos[0]
             except Exception as e: elo_max= -1
@@ -115,32 +117,36 @@ def get_data(start_year, end_year):
             try: elo_top24 = elos[23]
             except Exception as e: elo_top24= -1
 
-            elo_mean, elo_sd = sum(elos)/len(elos), statistics.pstdev(elos)
+            elo_mean, elo_sd = round(sum(elos)/len(elos), 2), round(statistics.pstdev(elos), 2)
             events.append([year, event, elo_max, elo_top8, elo_top24, elo_mean, elo_sd])
 
     events = pd.DataFrame(events, columns=["year", "event", "elo_max", "elo_top8", "elo_top24", "elo_mean", "elo_sd"])
 
     years = []
     for year in range(start_year, end_year+1):
-        teams = utils.loadTeams(year)
-        board, elos = sorted(teams.values()), []
-        for team in board: elos.append(team.get_rating_max())
+        teams_temp = utils.loadTeams(year)
+        board, elos = sorted(teams_temp.values()), []
+        for team in board: elos.append(round(team.get_rating_max(), 2))
         elo_max = elos[0]
         elo_1p = elos[int(0.01*len(elos))]
         elo_5p = elos[int(0.05*len(elos))]
         elo_10p = elos[int(0.10*len(elos))]
         elo_25p = elos[int(0.25*len(elos))]
         elo_median = elos[int(0.50*len(elos))]
-        elo_mean = sum(elos)/len(elos)
-        elo_sd = statistics.pstdev(elos)
+        elo_mean = round(sum(elos)/len(elos), 2)
+        elo_sd = round(statistics.pstdev(elos), 2)
         mse, acc = stats.getStats(year)
+        mse, acc = round(mse, 4), round(acc, 4)
         years.append([year, elo_max, elo_1p, elo_5p, elo_10p, elo_25p, elo_median, elo_mean, elo_sd, acc, mse])
 
     years = pd.DataFrame(years, columns=["year", "elo_max", "elo_1p", "elo_5p", "elo_10p", "elo_25p", "elo_median", "elo_mean", "elo_sd", "acc", "mse"])
     return team_matches, team_events, team_years, teams, events, years
 
-team_matches, team_events, team_years, teams, events, years = get_data(2002, 2020)
+start_year = 2002
+end_year = 2003
+team_matches, team_events, team_years, teams, events, years = get_data(start_year, end_year)
 
+'''
 print("TEAM_MATCHES")
 print(team_matches)
 print()
@@ -164,3 +170,7 @@ print()
 print("YEARS")
 print(years)
 print()
+'''
+
+team_matches_json = json.loads(team_matches.to_json(orient='records'))
+print(team_matches_json)
