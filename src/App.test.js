d3c2500bb02279c7a56b09cacaac3bfdfcbec321
Abhijit Gupta <avgupta456@gmail.com>
2020-05-20 15:46:48 -0400

    Update App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import { render } from '@testing-library/react';
+import ReactDOM from 'react-dom';
 import App from './App';
 
-test('renders Hello World', () => {
-  const { getByText } = render(<App />);
-  const linkElement = getByText(/Teams/i);
-  expect(linkElement).toBeInTheDocument();
+it('renders without crashing', () => {
+  const div = document.createElement('div');
+  ReactDOM.render(<App />, div);
 });
