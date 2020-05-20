9251947513869aa007a8d5473e76d51186886772
Abhijit Gupta <avgupta456@gmail.com>
2020-05-20 15:26:12 -0400

    int elo

--- rankings/models.py
+++ rankings/models.py
@@ -7,9 +7,9 @@ class TeamMatch(models.Model):
     event = models.CharField(max_length=10) #ex: ncwak
     match = models.CharField(max_length=10) #ex: sf1m1
     team = models.IntegerField() #ex: 5511
-    elo_start = models.DecimalField(max_digits=6, decimal_places=2) #ex: 1746.45
-    elo_end = models.DecimalField(max_digits=6, decimal_places=2) #ex: 1746.45
-    elo_diff = models.DecimalField(max_digits=6, decimal_places=2) #ex: 100
+    elo_start = models.IntegerField() #ex: 1746
+    elo_end = models.IntegerField() #ex: 1746
+    elo_diff = models.IntegerField() #ex: 100
 
     class Meta:
         unique_together = (("year", "event", "match", "team"))
@@ -19,12 +19,12 @@ class TeamEvent(models.Model):
     year = models.IntegerField() #ex: 2019
     event = models.CharField(max_length=10) #ex: ncwak
     team = models.IntegerField() #ex: 5511
-    elo_start = models.DecimalField(max_digits=6, decimal_places=2) #ex: 1746.45
-    elo_pre_playoffs = models.DecimalField(max_digits=6, decimal_places=2) #ex: 1746.45
-    elo_end = models.DecimalField(max_digits=6, decimal_places=2) #ex: 1746.45
-    elo_mean = models.DecimalField(max_digits=6, decimal_places=2) #ex: 1746.45
-    elo_max = models.DecimalField(max_digits=6, decimal_places=2) #ex: 1746.45
-    elo_diff = models.DecimalField(max_digits=6, decimal_places=2) #ex: 100
+    elo_start = models.IntegerField() #ex: 1746
+    elo_pre_playoffs = models.IntegerField() #ex: 1746
+    elo_end = models.IntegerField() #ex: 1746
+    elo_mean = models.IntegerField() #ex: 1746
+    elo_max = models.IntegerField() #ex: 1746
+    elo_diff = models.IntegerField() #ex: 100
 
     class Meta:
         unique_together = (("year", "event", "team"))
@@ -33,12 +33,12 @@ class TeamYear(models.Model):
     id = models.AutoField(primary_key=True)
     year = models.IntegerField() #ex: 2019
     team = models.IntegerField() #ex: 5511
-    elo_start = models.DecimalField(max_digits=6, decimal_places=2) #ex: 1746.45
-    elo_pre_champs = models.DecimalField(max_digits=6, decimal_places=2) #ex: 1746.45
-    elo_end = models.DecimalField(max_digits=6, decimal_places=2) #ex: 1746.45
-    elo_mean = models.DecimalField(max_digits=6, decimal_places=2) #ex: 1746.45
-    elo_max = models.DecimalField(max_digits=6, decimal_places=2) #ex: 1746.45
-    elo_diff = models.DecimalField(max_digits=6, decimal_places=2) #ex: 100
+    elo_start = models.IntegerField() #ex: 1746
+    elo_pre_champs = models.IntegerField() #ex: 1746
+    elo_end = models.IntegerField() #ex: 1746
+    elo_mean = models.IntegerField() #ex: 1746
+    elo_max = models.IntegerField() #ex: 1746
+    elo_diff = models.IntegerField() #ex: 100
     rank = models.IntegerField() #ex: 546
     percentile = models.DecimalField(max_digits=6, decimal_places=4) #ex: 5.24
 
@@ -47,31 +47,31 @@ class TeamYear(models.Model):
 
 class Team(models.Model):
     team = models.IntegerField(primary_key=True) #ex: 5511
-    elo = models.DecimalField(max_digits=6, decimal_places=2) #ex: 1700
+    elo = models.IntegerField() #ex: 1700
     elos = models.CharField(max_length=200, validators=[validate_comma_separated_integer_list]) #ex: [1500, 1600, 1400, ...]
-    elo_mean = models.DecimalField(max_digits=6, decimal_places=2) #ex: 1600
-    elo_max = models.DecimalField(max_digits=6, decimal_places=2) #ex: 1900
+    elo_mean = models.IntegerField() #ex: 1600
+    elo_max = models.IntegerField() #ex: 1900
     elo_max_year = models.IntegerField() #ex: 2015
 
 class Event(models.Model):
     id = models.AutoField(primary_key=True)
     year = models.IntegerField() #ex: 2019
     event = models.CharField(max_length=10) #ex: ncwak
-    elo_max = models.DecimalField(max_digits=6, decimal_places=2) #ex: 1900
-    elo_top8 = models.DecimalField(max_digits=6, decimal_places=2) #ex: 1800
-    elo_top24 = models.DecimalField(max_digits=6, decimal_places=2) #ex: 1700
-    elo_mean = models.DecimalField(max_digits=6, decimal_places=2) #ex: 1600
-    elo_sd = models.DecimalField(max_digits=6, decimal_places=2) #ex: 500
+    elo_max = models.IntegerField() #ex: 1900
+    elo_top8 = models.IntegerField() #ex: 1800
+    elo_top24 = models.IntegerField() #ex: 1700
+    elo_mean = models.IntegerField() #ex: 1600
+    elo_sd = models.IntegerField() #ex: 500
 
 class Year(models.Model):
     year = models.IntegerField(primary_key=True) #ex: 2019
-    elo_max = models.DecimalField(max_digits=6, decimal_places=2) #ex: 2000
-    elo_1p = models.DecimalField(max_digits=6, decimal_places=2) #ex: 1950
-    elo_5p = models.DecimalField(max_digits=6, decimal_places=2) #ex: 1900
-    elo_10p = models.DecimalField(max_digits=6, decimal_places=2) #ex: 1850
-    elo_25p = models.DecimalField(max_digits=6, decimal_places=2) #ex: 1800
-    elo_median = models.DecimalField(max_digits=6, decimal_places=2) #ex: 1550
-    elo_mean = models.DecimalField(max_digits=6, decimal_places=2) #ex: 1500
-    elo_sd = models.DecimalField(max_digits=6, decimal_places=2) #ex: 500
+    elo_max = models.IntegerField() #ex: 2000
+    elo_1p = models.IntegerField() #ex: 1950
+    elo_5p = models.IntegerField() #ex: 1900
+    elo_10p = models.IntegerField() #ex: 1850
+    elo_25p = models.IntegerField() #ex: 1800
+    elo_median = models.IntegerField() #ex: 1550
+    elo_mean = models.IntegerField() #ex: 1500
+    elo_sd = models.IntegerField() #ex: 500
     acc = models.DecimalField(max_digits=6, decimal_places=4) #ex: 0.70
     mse = models.DecimalField(max_digits=6, decimal_places=4) #ex: 0.17
