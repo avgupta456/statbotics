95615c6df737e3f48b25627d425808ae371b9679
Abhijit Gupta <avgupta456@gmail.com>
2020-05-22 17:25:07 -0400

    added some settings

--- src/App.js
+++ src/App.js
@@ -14,12 +14,67 @@ const App = () => {
     fetchMyAPI();
   }, []);
 
-  console.log(teams);
-  const data = teams.map(function(x){ return [x["team"], x["name"], x["elo"], x["elo_recent"], x["elo_mean"], x["elo_max"]] });
+  const columns = [
+    {
+      label: "Number",
+      options: {
+        filter: false,
+        sort: true,
+      }
+    },
+    {
+      label: "Name",
+      options: {
+        filter: false,
+        sort: true,
+      }
+    },
+    {
+      label: "ELO",
+      options: {
+        filter: false,
+        sort: true,
+        searchable: false,
+      }
+    },
+    {
+      label: "ELO Recent",
+      options: {
+        filter: false,
+        sort: true,
+        searchable: false,
+      }
+    },
+    {
+      label: "ELO Mean",
+      options: {
+        filter: false,
+        sort: true,
+        searchable: false,
+      }
+    },
+    {
+      label: "ELO Max",
+      options: {
+        filter: false,
+        sort: true,
+        searchable: false,
+      }
+    },
+  ]
+
+  const data = teams.map(function(x){ return [
+    x["team"],
+    <a href={`teams/${x["team"]}`}>{x["name"]}</a>,
+    x["elo"],
+    x["elo_recent"],
+    x["elo_mean"],
+    x["elo_max"]
+  ]});
 
   return (
     <div>
-    <ReactTable data={data}/>
+    <ReactTable columns={columns} data={data}/>
     </div>
   );
 }
--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -1,65 +1,15 @@
 import React from 'react';
 import MUIDataTable from "mui-datatables";
 
-export default function ReactTable({data}) {
+export default function ReactTable({columns, data}) {
 
   const options = {
-  filterType: 'checkbox',
-  resizeableColumns: true
+  filterType: 'multiselect',
+  responsive: 'scrollMaxHeight',
+  rowsPerPageOptions: [10, 20, 50],
+
   };
 
-  const columns = [
-    {
-      name: "Number",
-      label: "number",
-      options: {
-        filter: false,
-        sort: true,
-      }
-    },
-    {
-      name: "Name",
-      label: "name",
-      options: {
-        filter: false,
-        sort: true,
-      }
-    },
-    {
-      name: "ELO",
-      label: "elo",
-      options: {
-        filter: false,
-        sort: true,
-      }
-    },
-    {
-      name: "ELO Recent",
-      label: "elo_recent",
-      options: {
-        filter: false,
-        sort: true,
-      }
-    },
-    {
-      name: "ELO Mean",
-      label: "elo_mean",
-      options: {
-        filter: false,
-        sort: true,
-      }
-    },
-    {
-      name: "ELO Max",
-      label: "elo_max",
-      options: {
-        filter: false,
-        sort: true,
-      }
-    },
-  ]
-
-  console.log(data)
   return (
     <MUIDataTable
       title={"Team List"}
