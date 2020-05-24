6ada5170b7f0633e319ee738c5d1877e3e0d9c08
Abhijit Gupta <avgupta456@gmail.com>
2020-05-23 23:08:59 -0400

    useEffect!

--- src/api/index.js
+++ src/api/index.js
@@ -5,9 +5,29 @@ const url = 'https://backend.statbotics.io/api';
 export const fetchTeams = async () => {
   try {
     const teams = await axios.get(`${url}/teams/by/elo_recent`);
-    console.log(teams.data)
+    console.log(teams.data);
     return teams.data;
   } catch (error) {
     return error;
   }
 };
+
+export const fetchTeams_byRegion = async (region) => {
+  try {
+    const teams = await axios.get(`${url}/teams/region/${region}/by/elo_recent`);
+    console.log(teams.data);
+    return teams.data;
+  } catch (error) {
+    return error;
+  }
+}
+
+export const fetchTeams_byDistrict = async (district) => {
+  try {
+    const teams = await axios.get(`${url}/teams/district/${district}/by/elo_recent`);
+    console.log(teams.data);
+    return teams.data;
+  } catch (error) {
+    return error;
+  }
+}
--- src/components/Containers/TeamLookup/TeamLookup.js
+++ src/components/Containers/TeamLookup/TeamLookup.js
@@ -2,21 +2,20 @@ import React, { useState, useEffect } from "react";
 
 import Paper from '@material-ui/core/Paper';
 
+import { DropdownButton, Dropdown } from "react-bootstrap";
+
+import faker from 'faker';
+import _ from 'lodash';
+
 import { ReactTable } from './../../../components';
-import { fetchTeams } from './../../../api';
+import { fetchTeams, fetchTeams_byRegion, fetchTeams_byDistrict } from './../../../api';
 import styles from './TeamLookup.module.css';
 
 export default function TeamLookup() {
-  const [teams, setTeams] = useState([]);
-
-  useEffect(() => {
-    const fetchMyAPI = async () => {
-      const new_teams = await fetchTeams();
-      setTeams(new_teams.results);
-    };
-
-    fetchMyAPI();
-  }, []);
+  const [region, setRegion] = useState("None");
+  const [district, setDistrict] = useState("None");
+  const [format, setFormat] = useState("Teams");
+  const [data, setData] = useState([]);
 
   //column name, searchable, visible, filterable
   const columns = [
@@ -31,28 +30,92 @@ export default function TeamLookup() {
     ["Max ELO", false, true, false],
   ];
 
-  const data = teams.map(function(x, i){ return [
-    x["team"],
-    <a href={`teams/${x["team"]}`}>{x["name"]}</a>,
-    (x["active"]?"Active":"Inactive"),
-    x["district"],
-    x["region"],
-    x["elo"],
-    x["elo_recent"],
-    x["elo_mean"],
-    x["elo_max"],
-  ]});
+  function clean(teams) {
+    return teams.map(function(x, i){ return [
+      x["team"],
+      <a href={`teams/${x["team"]}`}>{x["name"]}</a>,
+      (x["active"]?"Active":"Inactive"),
+      x["district"],
+      x["region"],
+      x["elo"],
+      x["elo_recent"],
+      x["elo_mean"],
+      x["elo_max"],
+    ]});
+  }
 
-  const title = "Team Lookup"
+  useEffect(() => {
+    const getTeams = async () => {
+      console.log("getTeams")
+      const new_teams = await fetchTeams();
+      console.log(clean(new_teams.results))
+      setData(clean(new_teams.results));
+      console.log(data)
+    };
+
+    const getTeams_byRegion = async () => {
+      console.log("getTeams_byRegion")
+      const new_teams = await fetchTeams_byRegion(region);
+      console.log(new_teams.results)
+      setData(clean(new_teams.results));
+      console.log(data)
+    }
+
+    const getTeams_byDistrict = async () => {
+      console.log("getTeams_byDistrict")
+      const new_teams = await fetchTeams_byDistrict(district);
+      console.log(new_teams.results)
+      setData(clean(new_teams.results));
+      console.log(data)
+    }
+
+    if(format==="Teams") {
+      getTeams()
+    }
+    else if(format==="Region") {
+      getTeams_byRegion()
+    }
+    else {
+      getTeams_byDistrict()
+    }
+  }, [format, region, district]);
+
+
+  const addressDefinitions = faker.definitions.address
+  const stateOptions = _.map(addressDefinitions.state, (state, index) => ({
+    key: addressDefinitions.state_abbr[index],
+    text: state,
+  }));
+
+  function regionClick(key) {
+    setRegion(key);
+    setFormat("Region");
+  };
+
+  function districtClick(key) {
+    setDistrict(key);
+    setFormat("District");
+  }
 
   return (
     <div>
       <Paper
-        elevation={3}
+        elevation={0}
         className = {styles.body}
 
         children = {
-          <ReactTable title={title} columns={columns} data={data}/>
+          <dir>
+            <DropdownButton title="Select State">
+              {stateOptions.map( x => (<Dropdown.Item onClick={() => regionClick(x["key"])} key={x["key"]}>{x["text"]}</Dropdown.Item>))}
+            </DropdownButton>
+            <DropdownButton title="Select District">
+              {stateOptions.map( x => (<Dropdown.Item onClick={() => districtClick(x["key"])} key={x["key"]}>{x["text"]}</Dropdown.Item>))}
+            </DropdownButton>
+            <ReactTable title={"Team Lookup"} columns={columns} data={data}/>
+            <h1>{region}</h1>
+            <h1>{district}</h1>
+            <h1>{format}</h1>
+          </dir>
         }
       />
     </div>
