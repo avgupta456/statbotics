491204fbf6a811a020dd4f7e79088e29a0bd2640
Abhijit Gupta <avgupta456@gmail.com>
2020-05-27 23:49:37 -0400

    Update constants.py

--- backend/constants.py
+++ backend/constants.py
@@ -1,16 +1,29 @@
-# Imports the Google Cloud client library
-from google.cloud import datastore
+#accessed by settings.py
+CLOUDSQL_CONNECTION = ""
+CLOUDSQL_DATABASE = ""
+CLOUDSQL_PASSWORD = ""
+CLOUDSQL_USER = ""
+SECRET_KEY = ""
 
-# Instantiates a client
-client = datastore.Client()
+try:
+    import os
 
-def getEnvVar(name):
-    key = client.key("secret", name)
-    entry = client.get(key)
-    return entry["value"]
+    CLOUDSQL_CONNECTION = os.environ.get("CLOUDSQL_CONNECTION")
+    CLOUDSQL_DATABASE = os.environ.get("CLOUDSQL_DATABASE")
+    CLOUDSQL_PASSWORD = os.environ.get("CLOUDSQL_PASSWORD")
+    CLOUDSQL_USER = os.environ.get("CLOUDSQL_USER")
+    SECRET_KEY = os.environ.get("SECRET_KEY")
+except Exception as e:
+    from google.cloud import datastore
+    client = datastore.Client()
 
-CLOUDSQL_CONNECTION = getEnvVar("CLOUDSQL_CONNECTION")
-CLOUDSQL_DATABASE = getEnvVar("CLOUDSQL_DATABASE")
-CLOUDSQL_PASSWORD = getEnvVar("CLOUDSQL_PASSWORD")
-CLOUDSQL_USER = getEnvVar("CLOUDSQL_USER")
-SECRET_KEY = getEnvVar("SECRET_KEY")
+    def getEnvVar(name):
+        key = client.key("secret", name)
+        entry = client.get(key)
+        return entry["value"]
+
+    CLOUDSQL_CONNECTION = getEnvVar("CLOUDSQL_CONNECTION")
+    CLOUDSQL_DATABASE = getEnvVar("CLOUDSQL_DATABASE")
+    CLOUDSQL_PASSWORD = getEnvVar("CLOUDSQL_PASSWORD")
+    CLOUDSQL_USER = getEnvVar("CLOUDSQL_USER")
+    SECRET_KEY = getEnvVar("SECRET_KEY")
