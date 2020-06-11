6dd9f81f5b22f5a818ee75c6ae3b1824fdbbd8eb
Abhijit Gupta <avgupta456@gmail.com>
2020-06-10 22:38:59 -0400

    ready to deploy

--- backend/settings.py
+++ backend/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = constants.SECRET_KEY
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
