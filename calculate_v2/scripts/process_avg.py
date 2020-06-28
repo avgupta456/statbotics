6bc4779bccbf627853f97b615750a8b1970fa844
Abhijit Gupta <avgupta456@gmail.com>
2020-06-27 23:49:13 -0400

    calculate rp avgs

--- calculate_v2/scripts/process_avg.py
+++ calculate_v2/scripts/process_avg.py
@@ -10,8 +10,8 @@ def process(start_year, end_year, SQL_Write, SQL_Read):
         for event in SQL_Read.getEvents(year=year, week=1):
             matches.extend(event.matches)
         matches = sorted(matches)
-        scores, autos, teleops, ones, twos, endgames, fouls, no_fouls \
-            = [], [], [], [], [], [], [], []
+        scores, autos, teleops, ones, twos, endgames, fouls, no_fouls, \
+            rp_1s, rp_2s = [], [], [], [], [], [], [], [], [], []
         for match in matches:
             scores.extend([match.red_score, match.blue_score])
             autos.extend([match.red_auto, match.blue_auto])
@@ -21,6 +21,8 @@ def process(start_year, end_year, SQL_Write, SQL_Read):
             endgames.extend([match.red_endgame, match.blue_endgame])
             fouls.extend([match.red_fouls, match.blue_fouls])
             no_fouls.extend([match.red_no_fouls, match.blue_no_fouls])
+            rp_1s.extend([match.red_rp_1, match.blue_rp_1])
+            rp_2s.extend([match.red_rp_2, match.blue_rp_2])
 
         year_obj.score_mean = round(sum(scores)/len(scores), 2)
         year_obj.score_sd = round(statistics.pstdev(scores), 2)
@@ -31,6 +33,8 @@ def process(start_year, end_year, SQL_Write, SQL_Read):
         year_obj.endgame_mean = round(sum(endgames)/len(endgames), 2)
         year_obj.foul_mean = round(sum(fouls)/len(fouls), 2)
         year_obj.no_foul_mean = round(sum(no_fouls)/len(no_fouls), 2)
+        year_obj.rp_1_mean = round(sum(rp_1s)/len(rp_1s), 4)
+        year_obj.rp_2_mean = round(sum(rp_2s)/len(rp_2s), 4)
         print(year, len(matches))
         SQL_Write.commit()
 
