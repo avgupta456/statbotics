a2453e41980f101e6b495b60c7664bd7ebbdf699
Abhijit Gupta <avgupta456@gmail.com>
2020-07-03 15:08:55 -0400

    event_pred files from statbotics_elo

---
+++ event_pred/event_pred.py
@@ -0,0 +1,171 @@
+import random
+
+from event_pred.classes import sql, read
+from event_pred.models import opr as opr_model
+from event_pred.models import elo as elo_model
+from event_pred.helper import rps as rps_model
+from event_pred.helper import tiebreakers
+
+
+SQL_Read = read.SQL_Read(sql.SQL(clean=False, echo=False))
+
+
+def clean(num):
+    return round(float(num), 2)
+
+
+def getObjects(event_key, year):
+    event = SQL_Read.getEvent_byKey(str(year)+event_key)
+    quals = sorted(SQL_Read.getMatches(event=event.id, playoff=False))
+    sd_score = event.year.score_sd
+    return event, quals, sd_score
+
+
+def getDicts(event, quals):
+    oprs, ils = opr_model.opr_v2(event, quals, [])
+    elos = elo_model.get_elos(event, quals)
+    teams = oprs.keys()
+
+    rps = rps_model.get_rps(event, quals, teams)
+    ties = tiebreakers.get_tiebreakers(event, quals, teams)
+
+    return oprs, ils, elos, rps, ties, teams
+
+
+def getCurrStats(index, teams, oprs, ils, elos):
+    oprs_curr, ils_curr, elos_curr = {}, {}, {}
+    for team in teams:
+        oprs_curr[team] = oprs[team][index]
+        ils_curr[team] = ils[team][index]
+        elos_curr[team] = elos[team][index]
+    return oprs_curr, ils_curr, elos_curr
+
+
+def getPreds(index, quals, oprs_curr, elos_curr, ils_curr, year, sd_score):
+    team_matches = {}  # for each match i after index, red and blue teams
+    preds = {}  # for each match i after index , win_prob, red rps, blue rps
+    for i in range(index, len(quals)):
+        m = quals[i]
+        red, blue = m.getTeams()
+        red_score = sum([clean(oprs_curr[t][0]) for t in red])
+        blue_score = sum([clean(oprs_curr[t][0]) for t in blue])
+        red_elo = sum([clean(elos_curr[t]) for t in red])
+        blue_elo = sum([clean(elos_curr[t]) for t in blue])
+
+        elo_prob = elo_model.win_prob(red_elo, blue_elo)
+        elo_margin = elo_model.win_margin(red_elo, blue_elo, sd_score)
+        opr_prob = opr_model.win_prob(red_score, blue_score, year, sd_score)
+        win_prob = (elo_prob + opr_prob)/2
+        win_margin = (elo_margin + (red_score - blue_score))/2
+        red_score = (red_score + blue_score)/2 + win_margin/2
+        blue_score = (red_score + blue_score)/2 - win_margin/2
+
+        red_rp_1 = opr_model.rp_prob([ils_curr[t][0] for t in red])
+        red_rp_2 = opr_model.rp_prob([ils_curr[t][1] for t in red])
+        blue_rp_1 = opr_model.rp_prob([ils_curr[t][0] for t in blue])
+        blue_rp_2 = opr_model.rp_prob([ils_curr[t][1] for t in blue])
+
+        ties_pred = tiebreakers.getOPRTiebreakers(oprs_curr, red, blue, year)
+        red_tie, blue_tie = ties_pred
+
+        team_matches[i] = [red, blue]
+        preds[i] = [round(red_score), round(blue_score), round(win_prob, 2),
+                    round(red_rp_1, 2), round(red_rp_2, 2),
+                    round(blue_rp_1, 2), round(blue_rp_2, 2),
+                    round(red_tie, 2), round(blue_tie, 2)]
+
+    return team_matches, preds
+
+
+def meanSim(index, quals, teams, team_matches, preds, rps, ties):
+    rps_out, ties_out = {}, {}
+    for team in teams:
+        rps_out[team] = rps[team][index][0]
+        ties_out[team] = ties[team][index][0]
+    for i in range(index, len(quals)):
+        red, blue = team_matches[i]
+        red_rps = preds[i][2] * 2 + preds[i][3] + preds[i][4]
+        blue_rps = (1-preds[i][2]) * 2 + preds[i][5] + preds[i][6]
+        for team in teams:
+            if team in red:
+                rps_out[team] += red_rps
+                ties_out[team] += preds[i][7]
+            if team in blue:
+                rps_out[team] += blue_rps
+                ties_out[team] += preds[i][8]
+    return rps_out, ties_out
+
+
+def singleSim(index, quals, teams, team_matches, preds, rps, mean_ties):
+    rps_out, ranks_out = {}, {}
+    for team in teams:
+        rps_out[team] = rps[team][index][0]
+
+    for i in range(index, len(quals)):
+        red, blue = team_matches[i]
+        red_rps, blue_rps = 0, 0
+        if preds[i][2] > random.uniform(0, 1): red_rps += 2  # noqa 702
+        else: blue_rps += 2  # noqa 702
+        if preds[i][3] > random.uniform(0, 1): red_rps += 1  # noqa 702
+        if preds[i][4] > random.uniform(0, 1): red_rps += 1  # noqa 702
+        if preds[i][5] > random.uniform(0, 1): blue_rps += 1  # noqa 702
+        if preds[i][6] > random.uniform(0, 1): blue_rps += 1  # noqa 702
+        for team in red: rps_out[team] += red_rps  # noqa 702
+        for team in blue: rps_out[team] += blue_rps  # noqa 702
+
+    for team in teams: ranks_out[team] = [rps_out[team], mean_ties[team]]  # noqa 702
+    ranks_out = sorted(ranks_out, key=lambda k: (ranks_out[k][0], ranks_out[k][1]))[::-1]  # noqa 502
+    ranks_out = {ranks_out[i]: i+1 for i in range(len(ranks_out))}
+    return rps_out, ranks_out
+
+
+def indexSim(index, iterations, teams, quals, team_matches, preds, rps, ties):
+    mean_rps, mean_ties = meanSim(index, quals, teams, team_matches, preds, rps, ties)  # noqa 502
+
+    T = len(teams)
+    avg_rps, ranks = {}, {}
+    for team in teams:
+        avg_rps[team] = 0
+        ranks[team] = [0 for i in range(T)]
+
+    for i in range(iterations):
+        rps_ind, ranks_ind = singleSim(index, quals, teams, team_matches,
+                                       preds, rps, mean_ties)
+        for team in teams:
+            avg_rps[team] += rps_ind[team]
+            ranks[team][ranks_ind[team]-1] += 1
+
+    avg_ranks = {}
+    for team in teams:
+        avg_rps[team] /= iterations
+        ranks[team] = [freq/iterations for freq in ranks[team]]
+        avg_ranks[team] = 1 + sum([i * ranks[team][i] for i in range(T)])
+    return mean_rps, mean_ties, avg_rps, avg_ranks, ranks
+
+
+def quickSim(year, event_key):
+    event, quals, sd_score = getObjects(event_key, year)
+    oprs, ils, elos, rps, ties, teams = getDicts(event, quals)
+    out = {t: [] for t in teams}
+    for i in range(len(quals)+1):
+        oprs_c, ils_c, elos_c = getCurrStats(i, teams, oprs, ils, elos)
+        team_matches, preds = getPreds(i, quals, oprs_c, elos_c, ils_c, year, sd_score)  # noqa 502
+        mean_rps, mean_ties = meanSim(i, quals, teams, team_matches, preds, rps, ties)  # noqa 502
+        for team in teams:
+            out[team].append([mean_rps[team], mean_ties[team]])
+    return out
+
+
+def sim(year, event_key, iterations=100):
+    event, quals, sd_score = getObjects(event_key, year)
+    oprs, ils, elos, rps, ties, teams = getDicts(event, quals)
+    out = {t: [] for t in teams}
+    for i in range(len(quals)+1):
+        oprs_c, ils_c, elos_c = getCurrStats(i, teams, oprs, ils, elos)
+        team_matches, preds = getPreds(i, quals, oprs_c, elos_c, ils_c, year, sd_score)  # noqa 502
+        mean_rps, mean_ties, avg_rps, avg_ranks, ranks = indexSim(
+            i, iterations, teams, quals, team_matches, preds, rps, ties)
+        for team in teams:
+            out[team].append([mean_rps[team], mean_ties[team], avg_rps[team],
+                              avg_ranks[team], ranks[team]])
+    return out
---
+++ event_pred/models/opr.py
@@ -0,0 +1,237 @@
+import numpy as np
+import scipy.linalg
+
+
+def logistic(n):
+    return float(1/(1+np.e**(-4*(n-0.5))))
+
+
+def score(match, alliance):
+    if alliance == "red":
+        return [match.red_score]
+    return [match.blue_score]
+
+
+def event_score(event):
+    return [event.opr_start]
+
+
+def all(match, alliance):
+    if alliance == "red":
+        return [match.red_score, match.red_auto, match.red_teleop,
+                match.red_1, match.red_2, match.red_endgame, match.blue_fouls,
+                match.red_no_fouls]
+    return [match.blue_score, match.blue_auto, match.blue_teleop, match.blue_1,
+            match.blue_2, match.blue_endgame, match.red_fouls,
+            match.blue_no_fouls]
+
+
+def event_all(event):
+    return [event.opr_start, event.opr_auto, event.opr_teleop, event.opr_1,
+            event.opr_2, event.opr_endgame, event.opr_fouls,
+            event.opr_no_fouls]
+
+
+def computeOPR(input, output, year, mean_score):
+    try:
+        A = np.matmul(input.T, input)
+        Y = np.matmul(input.T, output)
+        out = scipy.linalg.lstsq(A, Y, overwrite_a=True, overwrite_b=True,
+                                 check_finite=True, lapack_driver='gelsy')[0]
+    except scipy.linalg.LinAlgError or scipy.linalg.ValueError:
+        # if singular (not enough matches, etc)
+        out = computeAverages(input, output, year)
+    # if highly unstable, handles foul oprs
+    if np.min(out) < -mean_score/(2 if year <= 2004 else 3):
+        out = computeAverages(input, output, year)
+    return out
+
+
+def computeAverages(input, output, year):
+    T, Y = input.shape[1], output.shape[1]  # teams in event
+    TM = (2 if year <= 2004 else 3)  # teams per alliance
+    out = np.zeros(shape=(T, Y))
+    for i in range(T):
+        locs = np.where(input[i][:] == 1)
+        if len(locs[0]) == 0:
+            out[i] = np.array([0 * Y])
+        else:
+            out[i] = np.mean(output[locs], axis=0)/TM
+    return out
+
+
+def get_base(event, quals, playoffs, func, event_func):
+    mean_score, year = event.year.score_mean, event.year.id
+    team_events = {}
+    for team_event in event.team_events:
+        team_events[team_event.team_id] = team_event
+
+    # case of only playoffs
+    if len(quals) == 0:
+        teams, out = set(), {}
+        for m in playoffs:
+            [teams.add(t) for t in m.getRed()]
+            [teams.add(t) for t in m.getBlue()]
+        teams = list(teams)
+        for t in teams:
+            out[t] = [event_func(team_events[t])]
+        return True, 0, 0, 0, 0, 0, out, 0, 0, 0, 0, 0
+
+    teams, out = set(), {}
+    for m in quals:
+        [teams.add(t) for t in m.getRed()]
+        [teams.add(t) for t in m.getBlue()]
+    teams = list(teams)
+
+    M, T, Y = len(quals), len(teams), len(event_func(team_events[teams[0]]))
+    input = np.zeros(shape=(2*M, T), dtype="float")
+    output = np.zeros(shape=(2*M, Y), dtype="float")
+    match_objs = quals
+
+    arr = []
+    for i in range(M):
+        red, blue = match_objs[i].getRed(), match_objs[i].getBlue()
+        red = [teams.index(t) for t in red]
+        blue = [teams.index(t) for t in blue]
+        arr.append([red, blue])
+
+    return False, team_events, teams, arr, input, output, \
+        out, T, M, match_objs, year, mean_score
+
+
+def get_OPR(event, quals, playoffs, func=all, event_func=event_all):
+    quick_stop, team_events, teams, arr, input, output, out, T, M, match_objs, \
+        year, mean_score = get_base(event, quals, playoffs, func, event_func)
+    if quick_stop: return out  # noqa 702
+
+    oprs = computeOPR(input, output, year, mean_score)
+    for i in range(T):
+        out[teams[i]] = [oprs[i]]
+
+    for i in range(M):
+        m = match_objs[i]
+        for t in arr[i][0]: input[2*i][t] = 1  # noqa 701
+        for t in arr[i][1]: input[2*i+1][t] = 1  # noqa 701
+        output[2*i] = np.array(func(m, "red"))
+        output[2*i+1] = np.array(func(m, "blue"))
+        oprs = computeOPR(input, output, year, mean_score)
+        [out[teams[j]].append(oprs[j]) for j in range(T)]
+    return out
+
+
+def get_xOPR(event, quals, playoffs, func=all, event_func=event_all):
+    quick_stop, team_events, teams, arr, input, output, out, T, M, match_objs, \
+        year, mean_score = get_base(event, quals, playoffs, func, event_func)
+    if quick_stop: return out  # noqa 702
+
+    for i in range(M):
+        for t in arr[i][0]: input[2*i][t] = 1  # noqa 701
+        for t in arr[i][1]: input[2*i+1][t] = 1  # noqa 701
+        red = [event_func(team_events[teams[t]]) for t in arr[i][0]]
+        blue = [event_func(team_events[teams[t]]) for t in arr[i][1]]
+        output[2*i] = np.sum(red, axis=0)
+        output[2*i+1] = np.sum(blue, axis=0)
+
+    oprs = computeOPR(input, output, year, mean_score)
+    for i in range(T):
+        out[teams[i]] = [oprs[i]]
+
+    for i in range(M):
+        m = match_objs[i]
+        output[2*i] = np.array(func(m, "red"))
+        output[2*i+1] = np.array(func(m, "blue"))
+        oprs = computeOPR(input, output, year, mean_score)
+        [out[teams[j]].append(oprs[j]) for j in range(T)]
+    return out
+
+
+def get_ixOPR(event, quals, playoffs, func=all, event_func=event_all):
+    quick_stop, team_events, teams, arr, input, output, out, T, M, match_objs, \
+        year, mean_score = get_base(event, quals, playoffs, func, event_func)
+    if quick_stop: return out  # noqa 702
+
+    for i in range(M):
+        for t in arr[i][0]: input[2*i][t] = 1  # noqa 701
+        for t in arr[i][1]: input[2*i+1][t] = 1  # noqa 701
+        red = [event_func(team_events[teams[t]]) for t in arr[i][0]]
+        blue = [event_func(team_events[teams[t]]) for t in arr[i][1]]
+        output[2*i] = np.sum(red, axis=0)
+        output[2*i+1] = np.sum(blue, axis=0)
+
+    oprs = computeOPR(input, output, year, mean_score)
+
+    for i in range(T):
+        out[teams[i]] = [oprs[i]]
+
+    iterations = 2  # experimentally chosen
+    for i in range(M):
+        m = match_objs[i]
+        output[2*i] = np.array(func(m, "red"))
+        output[2*i+1] = np.array(func(m, "blue"))
+        oprs = computeOPR(input, output, year, mean_score)
+        for j in range(iterations - 1):
+            temp = output.copy()
+            for k in range(i, M):
+                temp[2*k] = np.sum([oprs[i] for i in arr[k][0]], axis=0)
+                temp[2*k+1] = np.sum([oprs[i] for i in arr[k][1]], axis=0)
+            oprs = computeOPR(input, temp, year, mean_score)
+        for j in range(T):
+            out[teams[j]].append(oprs[j])
+    return out
+
+
+def get_ILS(event, quals):
+    teams, out = [], {}
+    for team_event in event.team_events:
+        teams.append(team_event.team_id)
+        out[teams[-1]] = np.zeros(shape=(len(quals)+1, 2))
+        curr = [team_event.ils_1_start, team_event.ils_2_start]
+        out[teams[-1]][0] = np.array(curr)
+
+    for i, m in enumerate(quals):
+        red, blue = m.getTeams()
+        adjust_red_1 = (m.red_rp_1 - logistic(sum([out[r][i][0] for r in red]))) / 10 # noqa 502
+        adjust_red_2 = (m.red_rp_2 - logistic(sum([out[r][i][1] for r in red]))) / 10 # noqa 502
+        adjust_blue_1 = (m.blue_rp_1 - logistic(sum([out[b][i][0] for b in blue]))) / 10 # noqa 502
+        adjust_blue_2 = (m.blue_rp_2 - logistic(sum([out[b][i][1] for b in blue]))) / 10 # noqa 502
+
+        for t in teams:
+            out[t][i+1] = out[t][i]
+            if t in red:
+                out[t][i+1][0] = max(-0.2, out[t][i][0]+adjust_red_1)
+                out[t][i+1][1] = max(-0.2, out[t][i][1]+adjust_red_2)
+            elif t in blue:
+                out[t][i+1][0] = max(-0.2, out[t][i][0]+adjust_blue_1)
+                out[t][i+1][1] = max(-0.2, out[t][i][1]+adjust_blue_2)
+
+    return out
+
+
+def opr_v1(event, quals, playoffs):
+    return get_ixOPR(event, quals, playoffs, score, event_score)
+
+
+def opr_v2(event, quals, playoffs):
+    OPRs = get_ixOPR(event, quals, playoffs, all, event_all)
+    ILS = get_ILS(event, quals)
+    return OPRs, ILS
+
+
+def opr_standalone(SQL_Read, event):
+    quals = sorted(SQL_Read.getMatches(event=event.id, playoff=False))
+    playoffs = sorted(SQL_Read.getMatches(event=event.id, playoff=True))
+    OPRs = get_ixOPR(event, quals, playoffs, all, event_all)
+    ILS = get_ILS(event, quals)
+    return OPRs, ILS
+
+
+def win_prob(red, blue, year, sd_score):
+    if isinstance(red, list): red = sum(red)  # noqa 701
+    if isinstance(red, dict): red = sum(red.values())  # noqa 701
+    if isinstance(blue, list): blue = sum(blue)  # noqa 701
+    if isinstance(blue, dict): blue = sum(blue.values())  # noqa 701
+    return 1/(10**(5/8*(blue-red)/sd_score)+1)
+
+
+def rp_prob(teams):
+    return logistic(sum(teams))
