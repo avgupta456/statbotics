3ccb39c40c2325292608c5162cfac8746f97a268
Abhijit Gupta <avgupta456@gmail.com>
2020-07-05 17:43:24 -0400

    added secure constants

---
+++ calculate_v2/push/constants.py
@@ -0,0 +1,34 @@
+from google.cloud import datastore
+import os
+
+
+def getEnvVar(client, name):
+    key = client.key("secret", name)
+    entry = client.get(key)
+    return entry["value"]
+
+
+# accessed by settings.py
+CLOUDSQL_CONNECTION = os.environ.get("CLOUDSQL_CONNECTION")
+CLOUDSQL_DATABASE = os.environ.get("CLOUDSQL_DATABASE")
+CLOUDSQL_PASSWORD = os.environ.get("CLOUDSQL_PASSWORD")
+CLOUDSQL_USER = os.environ.get("CLOUDSQL_USER")
+
+if CLOUDSQL_CONNECTION is None or \
+        CLOUDSQL_DATABASE is None or \
+        CLOUDSQL_PASSWORD is None or \
+        CLOUDSQL_USER is None:
+
+    client = datastore.Client()
+    CLOUDSQL_CONNECTION = getEnvVar(client, "CLOUDSQL_CONNECTION")
+    CLOUDSQL_DATABASE = getEnvVar(client, "CLOUDSQL_DATABASE")
+    CLOUDSQL_PASSWORD = getEnvVar(client, "CLOUDSQL_PASSWORD")
+    CLOUDSQL_USER = getEnvVar(client, "CLOUDSQL_USER")
+
+# comment out to use the production database
+CLOUDSQL_DATABASE += "2"
+
+print(CLOUDSQL_CONNECTION)
+print(CLOUDSQL_DATABASE)
+print(CLOUDSQL_PASSWORD)
+print(CLOUDSQL_USER)
