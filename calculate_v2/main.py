2034b856c2823913a91acc30d6f19b71839f8196
Abhijit Gupta <avgupta456@gmail.com>
2020-07-05 17:44:15 -0400

    push data to cloud sql

--- calculate_v2/main.py
+++ calculate_v2/main.py
@@ -7,13 +7,18 @@ from process import (
     process_opr,
 )
 
+from push import (
+    clean as push_clean
+)
+
 start_year = 2002
 end_year = 2020
 clean = False
 cache = True
 
 TBA, SQL, SQL_Read, SQL_Write = setup.setup(clean)
-# process_tba.main(start_year, end_year, TBA, SQL_Write, SQL_Read, clean, cache) # 14 minutes  # noqa 502
-# process_avg.main(start_year, end_year, TBA, SQL_Write, SQL_Read, clean) # 7 seconds  # noqa 502
-# process_elo.main(start_year, end_year, TBA, SQL_Write, SQL_Read, clean) # ~30 minutes  # noqa 502
+process_tba.main(start_year, end_year, TBA, SQL_Write, SQL_Read, clean, cache) # 14 minutes  # noqa 502
+process_avg.main(start_year, end_year, TBA, SQL_Write, SQL_Read, clean) # 7 seconds  # noqa 502
+process_elo.main(start_year, end_year, TBA, SQL_Write, SQL_Read, clean) # ~30 minutes  # noqa 502
 process_opr.main(start_year, end_year, TBA, SQL_Write, SQL_Read, clean)
+push_clean.push()
