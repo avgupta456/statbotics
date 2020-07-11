bd22e95f4b51daf7d3626d18c03f1dd1e58d9621
Abhijit Gupta <avgupta456@gmail.com>
2020-07-11 17:41:06 -0400

    Update year_views.py

--- rankings/views/year_views.py
+++ rankings/views/year_views.py
@@ -6,3 +6,39 @@ from drf_yasg.utils import swagger_auto_schema
 
 from rankings.serializers import YearSerializer
 from rankings.models import Year as YearModel
+
+
+@swagger_auto_schema(
+    method='GET', responses={200: openapi.Response("", YearSerializer)},
+    operation_description='Basic information and prediction stats for given year',  # noqa 502
+)
+@api_view(['GET'])
+def Year(request, year):
+    years = YearModel.objects.filter(year=year).all()
+    serializer = YearSerializer(years, many=True)
+    return Response(serializer.data)
+
+
+@swagger_auto_schema(
+    method='GET', responses={200: openapi.Response("", YearSerializer)},
+    operation_description='Basic information and prediction stats for all year 2002-Present',  # noqa 502
+)
+@api_view(['GET'])
+def Years(request):
+    years = YearModel.objects.all()
+    serializer = YearSerializer(years, many=True)
+    return Response(serializer.data)
+
+
+@swagger_auto_schema(
+    method='GET', responses={200: openapi.Response("", YearSerializer)},
+    operation_description="Basic information and prediction stats for all" +
+                          " years 2002-Present, ordered by metric. Option" +
+                          " are '-elo_acc', '-elo_mse', '-opr_acc'," +
+                          " '-opr_mse', '-mix_acc', '-mix_mse'",
+)
+@api_view(['GET'])
+def YearsByMetric(request, metric):
+    years = YearModel.objects.all().order_by(metric)
+    serializer = YearSerializer(years, many=True)
+    return Response(serializer.data)
