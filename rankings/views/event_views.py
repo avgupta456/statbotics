aa59dff5eada8a04dcf3cbc5b1e150957a297b6e
Abhijit Gupta <avgupta456@gmail.com>
2020-07-11 17:40:59 -0400

    Update event_views.py

--- rankings/views/event_views.py
+++ rankings/views/event_views.py
@@ -6,3 +6,77 @@ from drf_yasg.utils import swagger_auto_schema
 
 from rankings.serializers import EventSerializer
 from rankings.models import Event as EventModel
+
+
+@swagger_auto_schema(
+    method='GET', responses={200: openapi.Response("", EventSerializer)},
+    operation_description='Basic information and stats for specified event',
+)
+@api_view(['GET'])
+def Event(request, event):
+    events = EventModel.objects.filter(event=event).all()
+    serializer = EventSerializer(events, many=True)
+    return Response(serializer.data)
+
+
+@swagger_auto_schema(method='GET', auto_schema=None)
+@api_view(['GET'])
+def _Events(request, year=None, country=None, state=None, district=None, metric=None):  # noqa 502
+    events = EventModel.objects
+    if year is not None:
+        events = events.filter(year=year)
+    if country is not None:
+        events = events.filter(country=country)
+    if state is not None:
+        events = events.filter(state=state)
+    if district is not None:
+        events = events.filter(district=district)
+    events = events.all()
+    if metric is not None:
+        events = events.order_by(metric)
+    else:
+        events = events.order_by('time')
+    serializer = EventSerializer(events, many=True)
+    return Response(serializer.data)
+
+
+@swagger_auto_schema(
+    method='GET', responses={200: openapi.Response("", EventSerializer)},
+    operation_description='Basic information and stats for all event',
+)
+@api_view(['GET'])
+def Events(request):
+    return _Events(request._request)
+
+
+@swagger_auto_schema(
+    method='GET', responses={200: openapi.Response("", EventSerializer)},
+    operation_description="Basic information and stats for all event," +
+                          " ordered by metric. Options are '-elo_top8'," +
+                          " '-elo_top24', '-elo_mean', '-opr_top8'," +
+                          " '-opr_top24', '-opr_mean'",
+)
+@api_view(['GET'])
+def EventsByMetric(request, metric):
+    return _Events(request._request, metric=metric)
+
+
+@swagger_auto_schema(
+    method='GET', responses={200: openapi.Response("", EventSerializer)},
+    operation_description='Basic information and stats for all event in a given year',  # noqa 502
+)
+@api_view(['GET'])
+def EventsYear(request, year):
+    return _Events(request._request, year=year)
+
+
+@swagger_auto_schema(
+    method='GET', responses={200: openapi.Response("", EventSerializer)},
+    operation_description="Basic information and stats for all event in a given year," +  # noqa 502
+                          " ordered by metric. Options are '-elo_top8'," +
+                          " '-elo_top24', '-elo_mean', '-opr_top8'," +
+                          " '-opr_top24', '-opr_mean'",
+)
+@api_view(['GET'])
+def EventsYearByMetric(request, year, metric):
+    return _Events(request._request, year=year, metric=metric)
