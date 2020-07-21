7ddf133ae7ee9e39f47aa18ab7360666099c1aaa
Abhijit Gupta <avgupta456@gmail.com>
2020-07-21 16:40:29 -0400

    unittest

---
+++ python/tests/test_login.py
@@ -0,0 +1,16 @@
+import unittest
+import warnings
+
+from statbotics import main
+
+
+class TestLogin(unittest.TestCase):
+    def setUp(self):
+        warnings.simplefilter("ignore", ResourceWarning)
+
+    def tearDown(self):
+        warnings.simplefilter("default", ResourceWarning)
+
+    def test_create_statbotics(self):
+        sb = main.Statbotics()
+        return sb
