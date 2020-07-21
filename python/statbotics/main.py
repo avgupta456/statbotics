b68e58f5f9ce574cd9b4cab2422663369220c24b
Abhijit Gupta <avgupta456@gmail.com>
2020-07-20 23:01:09 -0400

    remove expensive queries

--- python/statbotics/main.py
+++ python/statbotics/main.py
@@ -1,12 +1,11 @@
-import requests
 import datetime
 
+import requests
+import validate
 from cachecontrol import CacheControl
 
-import validate
 
-
-class Statbotics():
+class Statbotics:
     def __init__(self):
         self.BASE_URL = "https://backend.statbotics.io"
         self.session = CacheControl(requests.Session())
@@ -16,26 +15,25 @@ class Statbotics():
     def getToken(self, retries=0):
         if retries > 2:
             raise UserWarning("Could not connect to Statbotics.io")
-        self.session.get(self.BASE_URL+"/admin/")
-        if 'csrftoken' not in self.session.cookies:
+        self.session.get(self.BASE_URL + "/admin/")
+        if "csrftoken" not in self.session.cookies:
             return self.getToken(retries + 1)
-        return self.session.cookies['csrftoken']
+        return self.session.cookies["csrftoken"]
 
     def login(self, token):
-        login_data = {
-            "csrfmiddlewaretoken": token,
-            "next": self.BASE_URL + "/admin/"
-        }
+        login_data = {"csrfmiddlewaretoken": token, "next": self.BASE_URL + "/admin/"}
         self.session.post(
             self.BASE_URL + "/admin/login/",
             data=login_data,
-            headers=dict(Referer=self.BASE_URL)
+            headers=dict(Referer=self.BASE_URL),
         )
 
     def _get(self, url, retry=0):
-        resp = self.session.get(self.BASE_URL+url)
-        if resp.status_code == 200: return resp.json()  # noqa 701
-        if retry < 1: self._get(url, retry=retry+1)  # noqa 701
+        resp = self.session.get(self.BASE_URL + url)
+        if resp.status_code == 200:
+            return resp.json()  # noqa 701
+        if retry < 1:
+            self._get(url, retry=retry + 1)  # noqa 701
         raise UserWarning("Invalid query, or traffic too high (try later)")
 
     def _negate(self, string):
@@ -47,10 +45,11 @@ class Statbotics():
 
     def getTeam(self, team):
         validate.checkType(team, "int", "team")
-        return self._get("/api/team/"+str(team))
+        return self._get("/api/team/" + str(team))
 
-    def getTeams(self, country=None, state=None, district=None,
-                 active=True, metric=None):
+    def getTeams(
+        self, country=None, state=None, district=None, active=True, metric=None
+    ):
 
         url = "/api/teams"
 
@@ -70,7 +69,7 @@ class Statbotics():
 
     def getYear(self, year):
         validate.checkType(year, "int", "year")
-        out = self._get("/api/year/"+str(year))
+        out = self._get("/api/year/" + str(year))
         if len(out) == 0:
             raise ValueError("Invalid year")
         return out[0]
@@ -82,19 +81,35 @@ class Statbotics():
     def getTeamYear(self, team, year):
         validate.checkType(team, "int", "team")
         validate.checkType(year, "int", "year")
-        out = self._get("/api/team_year/team/"+str(team)+"/year/"+str(year))
+        out = self._get("/api/team_year/team/" + str(team) + "/year/" + str(year))
         if len(out) == 0:
             raise ValueError("Invalid (team, year) pair")
         return out[0]
 
-    def getTeamYears(self, team=None, year=None, country=None, state=None,
-                     district=None, metric=None, page=None):
+    def getTeamYears(
+        self,
+        team=None,
+        year=None,
+        country=None,
+        state=None,
+        district=None,
+        metric=None,
+        page=None,
+    ):
 
         url = "/api/team_years"
 
         validate.checkType(team, "int", "team")
         validate.checkType(year, "int", "year")
         validate.checkType(metric, "str", "metric")
+
+        if (
+            not year
+            and (country == "USA" or not country)
+            and not state
+            and not district
+        ):
+            raise UserWarning("Query too large, be more specific")
 
         if team and year:
             raise UserWarning("Use getTeamYear() instead")
@@ -121,13 +136,21 @@ class Statbotics():
 
     def getEvent(self, event):
         validate.checkType(event, "str", "event")
-        out = self._get("/api/event/"+event)
+        out = self._get("/api/event/" + event)
         if len(out) == 0:
             raise ValueError("Invalid event key")
         return out[0]
 
-    def getEvents(self, year=None, country=None, state=None, district=None,
-                  type=None, week=None, metric=None):
+    def getEvents(
+        self,
+        year=None,
+        country=None,
+        state=None,
+        district=None,
+        type=None,
+        week=None,
+        metric=None,
+    ):
 
         url = "/api/events"
 
@@ -157,14 +180,24 @@ class Statbotics():
     def getTeamEvent(self, team, event):
         validate.checkType(team, "int", "team")
         validate.checkType(event, "str", "event")
-        out = self._get("/api/team_event/team/"+str(team)+"/event/"+event)
+        out = self._get("/api/team_event/team/" + str(team) + "/event/" + event)
         if len(out) == 0:
             raise ValueError("Invalid (team, event) pair)")
         return out[0]
 
-    def getTeamEvents(self, team=None, year=None, event=None, country=None,
-                      state=None, district=None, type=None, week=None,
-                      metric=None, page=1):
+    def getTeamEvents(
+        self,
+        team=None,
+        year=None,
+        event=None,
+        country=None,
+        state=None,
+        district=None,
+        type=None,
+        week=None,
+        metric=None,
+        page=1,
+    ):
 
         url = "/api/team_events"
 
@@ -174,6 +207,17 @@ class Statbotics():
         validate.checkType(page, "int", "page")
         validate.checkType(week, "int", "week")
         type = validate.getType(type)
+
+        if (
+            not team
+            and not event
+            and (country == "USA" or not country)
+            and not state
+            and not district
+            and not (year and type)
+            and not (year and week)
+        ):
+            raise UserWarning("Query too large, be more specific")
 
         if team and event:
             raise UserWarning("Use getTeamEvent() instead")
@@ -211,7 +255,7 @@ class Statbotics():
 
     def getMatch(self, match):
         validate.checkType(match, "str", "match")
-        out = self._get("/api/match/"+match)
+        out = self._get("/api/match/" + match)
         if len(out) == 0:
             raise ValueError("Invalid match key")
         return out[0]
@@ -224,6 +268,9 @@ class Statbotics():
         validate.checkType(event, "str", "event")
         validate.checkType(page, "int", "page")
         validate.checkType(elims, "bool", "elims")
+
+        if not event:
+            raise UserWarning("Query too large, be more specific (event)")
 
         if year and event:
             raise UserWarning("Year input will be ignored")
@@ -245,13 +292,14 @@ class Statbotics():
     def getTeamMatch(self, team, match):
         validate.checkType(team, "int", "team")
         validate.checkType(match, "str", "match")
-        out = self._get("/api/team_match/team/"+str(team)+"/match/"+match)
+        out = self._get("/api/team_match/team/" + str(team) + "/match/" + match)
         if len(out) == 0:
             raise ValueError("Invalid (team, match) pair)")
         return out[0]
 
-    def getTeamMatches(self, team=None, year=None, event=None, match=None,
-                       elims=None, page=None):
+    def getTeamMatches(
+        self, team=None, year=None, event=None, match=None, elims=None, page=None
+    ):
         start = datetime.datetime.now()
         url = "/api/team_matches"
 
@@ -261,6 +309,11 @@ class Statbotics():
         validate.checkType(match, "str", "match")
         validate.checkType(page, "int", "page")
         validate.checkType(elims, "bool", "elims")
+
+        if not team and not event and not match:
+            raise UserWarning(
+                "Query too large, be more specific (team, event, or match)"
+            )
 
         if (year and event) or (year and match) or (event and match):
             raise UserWarning("Only specify one of (year, event, match)")
@@ -285,10 +338,10 @@ class Statbotics():
 
         out = self._get(url)
         end = datetime.datetime.now()
-        print(end-start)
+        print(end - start)
         return out
 
-    '''
+    """
     def getEventSim(self, event, index=None, full=False, iterations=None):
         validate.checkType(event, "str", "event")
         validate.checkType(index, "int", "index")
@@ -308,10 +361,4 @@ class Statbotics():
             url += "/simple"
 
         return self._get(url)
-    '''
-
-
-sb = Statbotics()
-for i in range(100):
-    print(len(sb.getTeamMatches()))
-    print(i)
+    """
