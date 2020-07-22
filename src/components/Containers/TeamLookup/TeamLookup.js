290ed2e704419a2e6ab4cb9a900b1530503b3309
Abhijit Gupta <avgupta456@gmail.com>
2020-07-22 09:55:44 -0400

    remove faker

--- src/components/Containers/TeamLookup/TeamLookup.js
+++ src/components/Containers/TeamLookup/TeamLookup.js
@@ -4,9 +4,6 @@ import { Paper, Typography } from "@material-ui/core";
 import { ButtonGroup, Button } from "react-bootstrap";
 import Select from "react-select";
 
-import faker from "faker";
-import _ from "lodash";
-
 import { ReactTable } from "./../../../components";
 
 import {
@@ -115,13 +112,59 @@ export default function TeamLookup() {
     { value: "Switzerland", label: "Switzerland" },
   ];
 
-  const addressDefinitions = faker.definitions.address;
-  const usaOptions = _.map(addressDefinitions.state, (state, index) => ({
-    value: addressDefinitions.state_abbr[index],
-    label: state,
-  }));
-
-  usaOptions.unshift({ value: "All", label: "All" });
+  const usaOptions = [
+    { value: "All", label: "All" },
+    { value: "AL", label: "Alabama" },
+    { value: "AK", label: "Alaska" },
+    { value: "AZ", label: "Arizona" },
+    { value: "AR", label: "Arkansas" },
+    { value: "CA", label: "California" },
+    { value: "CO", label: "Colorado" },
+    { value: "CT", label: "Connecticut" },
+    { value: "DE", label: "Delaware" },
+    { value: "FL", label: "Florida" },
+    { value: "GA", label: "Georgia" },
+    { value: "HI", label: "Hawaii" },
+    { value: "ID", label: "Idaho" },
+    { value: "IL", label: "Illinois" },
+    { value: "IN", label: "Indiana" },
+    { value: "IA", label: "Iowa" },
+    { value: "KS", label: "Kansas" },
+    { value: "KY", label: "Kentucky" },
+    { value: "LA", label: "Louisiana" },
+    { value: "ME", label: "Maine" },
+    { value: "MD", label: "Maryland" },
+    { value: "MA", label: "Massachusetts" },
+    { value: "MI", label: "Michigan" },
+    { value: "MN", label: "Minnesota" },
+    { value: "MS", label: "Mississippi" },
+    { value: "MO", label: "Missouri" },
+    { value: "MT", label: "Montana" },
+    { value: "NE", label: "Nebraska" },
+    { value: "NV", label: "Nevada" },
+    { value: "NH", label: "New Hampshire" },
+    { value: "NJ", label: "New Jersey" },
+    { value: "NM", label: "New Mexico" },
+    { value: "NY", label: "New York" },
+    { value: "NC", label: "North Carolina" },
+    { value: "ND", label: "North Dakota" },
+    { value: "OH", label: "Ohio" },
+    { value: "OK", label: "Oklahoma" },
+    { value: "OR", label: "Oregon" },
+    { value: "PA", label: "Pennsylvania" },
+    { value: "RI", label: "Rhode Island" },
+    { value: "SC", label: "South Carolina" },
+    { value: "SD", label: "South Dakota" },
+    { value: "TN", label: "Tennessee" },
+    { value: "TX", label: "Texas" },
+    { value: "UT", label: "Utah" },
+    { value: "VT", label: "Vermont" },
+    { value: "VA", label: "Virginia" },
+    { value: "WA", label: "Washington" },
+    { value: "WV", label: "West Virginia" },
+    { value: "WI", label: "Wisconsin" },
+    { value: "WY", label: "Wyoming" },
+  ]
 
   const canadaOptions = [
     { value: "All", label: "All" },
