0a2105b9b2e615a8a2e4bc140bbc7a034b030d22
Abhijit Gupta <avgupta456@gmail.com>
2020-07-25 10:23:04 -0400

    prep for submodules

--- statbotics_backend/rankings/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class RankingsConfig(AppConfig):
-    name = "rankings"
--- statbotics_calc/calculate/scripts/template.py
+++
@@ -1,9 +0,0 @@
-from helper import setup
-
-start_year = 2002
-end_year = 2020
-clean = False
-
-TBA, SQL, SQL_Read, SQL_Write = setup.setup(clean)
-
-# your code here
