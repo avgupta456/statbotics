6d854dbff2cdb7a60662a72b3bb74d3d107d62c5
Abhijit Gupta <avgupta456@gmail.com>
2020-08-02 18:48:01 -0400

    Create Bar.js

---
+++ src/components/Containers/EventView/Bar.js
@@ -0,0 +1,94 @@
+import React from "react";
+
+import { ResponsiveBar } from "@nivo/bar";
+
+import styles from "./EventView.module.css";
+
+class BarChart extends React.Component {
+  render() {
+    var data = this.props.data;
+
+    return (
+      <div className={styles.gray}>
+        <ResponsiveBar
+          data={data}
+          keys={["Auto OPR", "Teleop OPR", "Endgame OPR"]}
+          indexBy="team"
+          margin={{ top: 50, right: 130, bottom: 50, left: 60 }}
+          padding={0.3}
+          groupMode="stacked"
+          colors={{ scheme: "set1" }}
+          defs={[
+            {
+              id: "dots",
+              type: "patternDots",
+              background: "inherit",
+              color: "#38bcb2",
+              size: 4,
+              padding: 1,
+              stagger: true,
+            },
+            {
+              id: "lines",
+              type: "patternLines",
+              background: "inherit",
+              color: "#eed312",
+              rotation: -45,
+              lineWidth: 6,
+              spacing: 10,
+            },
+          ]}
+          borderColor={{ from: "color", modifiers: [["darker", 1.6]] }}
+          axisTop={null}
+          axisRight={null}
+          axisBottom={{
+            tickSize: 5,
+            tickPadding: 5,
+            tickRotation: 0,
+            legend: "Team",
+            legendPosition: "middle",
+            legendOffset: 32,
+          }}
+          axisLeft={{
+            tickSize: 5,
+            tickPadding: 5,
+            tickRotation: 0,
+            legend: "OPR",
+            legendPosition: "middle",
+            legendOffset: -40,
+          }}
+          enableLabel={false}
+          legends={[
+            {
+              dataFrom: "keys",
+              anchor: "bottom-right",
+              direction: "column",
+              justify: false,
+              translateX: 120,
+              translateY: 0,
+              itemsSpacing: 2,
+              itemWidth: 100,
+              itemHeight: 20,
+              itemDirection: "left-to-right",
+              itemOpacity: 0.85,
+              symbolSize: 20,
+              effects: [
+                {
+                  on: "hover",
+                  style: {
+                    itemOpacity: 1,
+                  },
+                },
+              ],
+            },
+          ]}
+          animate={true}
+          motionStiffness={90}
+          motionDamping={15}
+        />
+      </div>
+    );
+  }
+}
+
+export default BarChart;
