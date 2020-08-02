7999481a1fc9dad01076f683fc22fe2a54350ee7
Abhijit Gupta <avgupta456@gmail.com>
2020-08-02 12:54:36 -0400

    retry fetch sim up to twice

--- src/api/event_sim_api.js
+++ src/api/event_sim_api.js
@@ -4,11 +4,15 @@ import { url } from "./index";
 
 /*TEAM EVENT API CALLS*/
 
-export const fetchSimFull = async (key) => {
+export const fetchSimFull = async (key, retries) => {
   try {
     const sim = await axios.get(`${url}/event_sim/event/${key}/full`);
     return sim.data;
   } catch (error) {
-    return error;
+    if (retries > 1) {
+      return [];
+    } else {
+      return fetchSimFull(key, retries + 1);
+    }
   }
 };
