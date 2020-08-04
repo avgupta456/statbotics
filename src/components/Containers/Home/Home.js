81c5db337ba872fa6a703e6b37ecca381ea5f1a7
Abhijit Gupta <avgupta456@gmail.com>
2020-08-04 17:21:45 -0400

    cleanup

--- src/components/Containers/Home/Home.js
+++ src/components/Containers/Home/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 
-import { Card, Button } from "react-bootstrap";
+import { Card } from "react-bootstrap";
 import { Typography } from "@material-ui/core";
 
 import { Header } from "./../..";
