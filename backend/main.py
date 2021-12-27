2423ca6fb3b6adb06bd0ee7a859024f675d0869b
Abhijit Gupta <avgupta456@gmail.com>
2021-12-27 13:07:54 -0500

    move main.py to /backend

--- backend/main.py
+++ backend/main.py
@@ -1,4 +1,4 @@
-from backend.wsgi import application
+from src.backend.wsgi import application
 
 # App Engine by default looks for a main.py file at the root of the app
 # directory with a WSGI-compatible object called app.
