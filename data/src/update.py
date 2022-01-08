ca54f1103c7d1e4e0d49159d67dd460ba8f67c59
Abhijit Gupta <avgupta456@gmail.com>
2022-01-08 00:07:05 -0500

    Set Random Seed

--- data/src/update.py
+++ data/src/update.py
@@ -1,10 +1,14 @@
 import os
+import random
 
 from dotenv import load_dotenv
 
 load_dotenv()
 
 os.environ["LOCAL_DB"] = "True"
+
+# Set random seed, for random team matches
+random.seed(5511)
 
 from process.process_main import process_main  # noqa: E402
 
