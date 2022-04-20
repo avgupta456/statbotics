eecc79779b306a7f57b0d7b1e01ac3aeea8c3e50
Abhijit Gupta <avgupta456@gmail.com>
2022-04-19 21:14:40 -0400

    Update read_tba.py

--- data/src/tba/read_tba.py
+++ data/src/tba/read_tba.py
@@ -135,6 +135,10 @@ def get_team_events(event: str, cache: bool = True) -> List[Dict[str, Any]]:
     if event == "2022isde3":
         out[7177] = {"team": 7177, "rank": -1}
 
+    if event == "2022micmp":
+        for team in [5612, 2767, 503]:
+            out[team] = {"team": team, "rank": -1}
+
     # queries TBA for rankings, some older events are not populated
     try:
         rankings = get_tba("event/" + str(event) + "/rankings", cache=cache)["rankings"]
