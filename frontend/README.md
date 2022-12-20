8d0e8a22c2dcff237766c2a002765f0b5f2a96bc
Abhijit Gupta <avgupta456@gmail.com>
2022-12-20 14:10:48 -0500

    Start NextJS

---
+++ frontend/README.md
@@ -0,0 +1,3 @@
+# Statbotics.io Frontend
+
+WIP
---
+++ frontend/app/page.tsx
@@ -0,0 +1,12 @@
+export default function Home() {
+  return (
+    <div className="bg-blue-100">
+      <p className="text-green-500">
+        Welcome to <a href="https://nextjs.org">Next.js!</a>
+      </p>
+      <p className="description">
+        Get started by editing <code>pages/index.js</code>
+      </p>
+    </div>
+  );
+}
