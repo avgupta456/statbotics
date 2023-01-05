404330a3a7f76c6e600d92ea04f8b2f563648ac4
Abhijit Gupta <avgupta456@gmail.com>
2023-01-04 19:11:08 -0500

    Use shared layout

--- frontend/app/(site)/shared/layout.tsx
+++ frontend/app/(site)/shared/layout.tsx
@@ -5,7 +5,23 @@ import Select from "react-select";
 
 import { yearOptions } from "../../../components/filterConstants";
 
-const PageLayout = ({ title, year, setYear, children }) => {
+const PageLayout = ({
+  title,
+  year,
+  setYear,
+  minYear,
+  children,
+}: {
+  title: string;
+  year: number;
+  setYear: (year: number) => void;
+  minYear?: number;
+  children: React.ReactNode;
+}) => {
+  const filteredYearOptions = yearOptions.filter(
+    (option) => parseInt(option.value) >= (minYear ?? 2002)
+  );
+
   return (
     <div className="w-full h-full flex-grow flex flex-col pt-4 md:pt-8 md:pb-4 md:px-4">
       <div className="w-full h-full flex-grow flex flex-col">
@@ -16,7 +32,7 @@ const PageLayout = ({ title, year, setYear, children }) => {
             styles={{
               menu: (provided) => ({ ...provided, zIndex: 9999 }),
             }}
-            options={yearOptions}
+            options={filteredYearOptions}
             onChange={(e) => setYear((e?.value ?? 2023) as number)}
             value={{
               value: year.toString(),
