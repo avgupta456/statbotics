4b165a038756a0c3eb2069ea30481fa60c0e59a5
Abhijit Gupta <avgupta456@gmail.com>
2023-01-09 00:27:40 -0500

    Create page.tsx

---
+++ frontend/app/(docs)/blog/v2/page.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+// TODO: abstract out page format
+// TODO: blog root page
+
+const Page = () => {
+  return (
+    <div className="w-4/5 mx-auto">
+      <div className="w-full h-full py-16 prose prose-slate max-w-none">
+        <h2 className="w-full text-center">Statbotics V2</h2>
+        <p className="lead">TODO</p>
+        <h3>Welcome to Statbotics V2!</h3>
+        <h3>What&apos;s new?</h3>
+        <h3>Why a rewrite?</h3>
+        <h3>What&apos;s next?</h3>
+      </div>
+    </div>
+  );
+};
+
+export default Page;
