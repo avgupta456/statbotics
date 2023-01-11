44e007abb35f2e469eec4125567c74b5d0155365
Abhijit Gupta <avgupta456@gmail.com>
2023-01-11 14:44:04 -0500

    Deploy to Prod (#161)

--- frontend/app/head.tsx
+++ frontend/app/head.tsx
@@ -1,5 +1,3 @@
-/* eslint-disable @next/next/next-script-for-ga */
-
 export default function Head() {
   return (
     <>
@@ -9,30 +7,12 @@ export default function Head() {
       <meta name="title" content="Statbotics" />
       <meta name="description" content="Modernizing FRC Data Analytics" />
       <meta name="og:type" content="website" />
-      <meta name="og:url" content="https://www.v2.statbotics.io" />
+      <meta name="og:url" content="https://www.statbotics.io" />
       <meta name="og:title" content="Statbotics" />
       <meta name="og:description" content="Modernizing FRC Data Analytics" />
-      <meta name="og:image" content="https://www.v2.statbotics.io/spline.png" />
+      <meta name="og:image" content="https://www.statbotics.io/spline.png" />
 
       <link rel="icon" href="/favicon.ico" />
-
-      <script async src="https://www.googletagmanager.com/gtag/js?id=UA-145490100-3" />
-      <script
-        id="gtag"
-        dangerouslySetInnerHTML={{
-          __html: `window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'UA-145490100-3', { page_path: window.location.pathname });
-        `,
-        }}
-      />
-      <script
-        async
-        defer
-        data-website-id="eaf23a65-20a8-40aa-a1a6-730683f5fe10"
-        src="https://umami-nine-zeta.vercel.app/umami.js"
-      />
     </>
   );
 }
