177b173a400f93f25ee561ab65fe19378af8ce70
Abhijit Gupta <avgupta456@gmail.com>
2023-01-13 19:33:33 -0500

    Delete V1 (#172)

--- v1/data/README.md
+++
@@ -1,3 +0,0 @@
-# Statbotics Data
-
-Statbotics aims to create and distribute modern data analytics for the FIRST Robotics Competition. This folder includes models (ELO, OPR, ILS, etc) that calculate the datasets Statbotics uses.
--- v1/frontend/src/components/index.js
+++
@@ -1,6 +0,0 @@
-export { default as ReactTable } from "./Table/Table";
-export { default as LineChart } from "./LineChart/LineChart";
-export { default as Header } from "./Header/Header";
-export { default as TeamSelect } from "./TeamSelect/TeamSelect";
-export { default as Navigation } from "./Navigation/Navigation";
-export { default as useWindowDimensions } from "./WindowDimensions/WindowDimensions";
