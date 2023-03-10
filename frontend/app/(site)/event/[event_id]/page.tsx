d7feac94108a9372569869cdc4a14621a6d38f88
Abhijit Gupta <avgupta456@gmail.com>
2023-03-10 17:39:46 -0500

    Link TBA on Events

--- frontend/app/(site)/event/[event_id]/page.tsx
+++ frontend/app/(site)/event/[event_id]/page.tsx
@@ -1,4 +1,8 @@
 import React from "react";
+import { BsTwitch } from "react-icons/bs";
+
+import Image from "next/image";
+import Link from "next/link";
 
 import { BACKEND_URL } from "../../../../constants";
 import { log, round, truncate } from "../../../../utils";
@@ -30,14 +34,34 @@ async function Page({ params }: { params: { event_id: string } }) {
   }
 
   let truncatedEventName = truncate(data.event.name, 30);
+  const status = data.event.status;
 
   return (
     <div className="w-full h-full flex-grow flex flex-col pt-4 md:pt-8 md:pb-4 md:px-4">
       <div className="h-full h-full flex-grow flex flex-col">
-        <div className="w-full flex flex-row items-end justify-center mb-4">
-          <p className="text-2xl lg:text-3xl">
+        <div className="w-full flex items-center justify-center mb-4">
+          <p className="text-2xl lg:text-3xl mr-6">
             {data.year.year} {truncatedEventName}
           </p>
+
+          <Link
+            href={"https://www.thebluealliance.com/event/" + event_id}
+            rel="noopener noreferrer"
+            target="_blank"
+          >
+            <Image src="/tba.png" alt="TBA" height={24} width={24} />
+          </Link>
+          {status === "Ongoing" && (
+            <Link
+              href={"https://www.thebluealliance.com/gameday/" + event_id}
+              rel="noopener noreferrer"
+              target="_blank"
+              className="ml-2 text-sm"
+              style={{ color: "#6441a5" }}
+            >
+              <BsTwitch className="text-2xl" />
+            </Link>
+          )}
         </div>
         <Tabs eventId={event_id} year={data.year.year} data={data} />
       </div>
