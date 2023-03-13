c67833c31fa2fb330250a7d8360baa18f497ed83
Abhijit Gupta <avgupta456@gmail.com>
2023-03-13 01:46:11 -0400

    Basic UI

--- frontend/app/(site)/matches/upcoming.tsx
+++ frontend/app/(site)/matches/upcoming.tsx
@@ -2,19 +2,42 @@ import React from "react";
 
 import MatchTable from "../../../components/MatchTable";
 import { APIMatch } from "../../../components/types/api";
+import { formatMatch } from "../../../components/utils";
+import { truncate } from "../../../utils";
 import { MatchData } from "../types";
 
 const UpcomingMatches = ({ data }: { data: MatchData }) => {
   return (
-    <div>
-      {data.upcoming_matches.map((match) => {
-        return (
-          <div key={match.match.key} className="w-full p-4 m-4 rounded">
-            <div className="text-center text-2xl font-bold">{match.event_name}</div>
-            <MatchTable year={2023} teamNum={0} matches={[match.match]} foulRate={0} />
-          </div>
-        );
-      })}
+    <div className="flex flex-col gap-8">
+      {data.upcoming_matches
+        .sort((a, b) => a.match.predicted_time - b.match.predicted_time)
+        .map((match) => {
+          return (
+            <div key={match.match.key} className="w-full">
+              <div className="w-full flex flex-wrap justify-center text-lg mb-2">
+                <p className="font-bold mr-1">{truncate(match.event_name, 30)}</p>
+                {" - "}
+                {formatMatch(
+                  match.match.comp_level,
+                  match.match.match_number,
+                  match.match.set_number
+                )}
+              </div>
+              <div className="w-full md:w-2/3 lg:w-1/2 overflow-x-scroll scrollbar-hide mx-auto">
+                <MatchTable
+                  year={2023}
+                  teamNum={0}
+                  matches={[match.match]}
+                  foulRate={0}
+                  showHeaders={true}
+                  showSubHeaders={false}
+                  showVideo={false}
+                  stacked={true}
+                />
+              </div>
+            </div>
+          );
+        })}
     </div>
   );
 };
--- frontend/components/utils.tsx
+++ frontend/components/utils.tsx
@@ -6,3 +6,28 @@ export const formatNumber = (num: number) => {
   }
   return num.toString();
 };
+
+export const compLevelFullNames = {
+  qm: "Qualifications",
+  ef: "Eighth Finals",
+  qf: "Quarterfinals",
+  sf: "Semifinals",
+  f: "Finals",
+};
+
+export const compLevelShortNames = {
+  qm: "Quals",
+  ef: "Eighths",
+  qf: "Quarters",
+  sf: "Semis",
+  f: "Finals",
+};
+
+export const formatMatch = (compLevel: string, matchNum: number, setNum: number) => {
+  let displayMatch = `${compLevelShortNames[compLevel]} ${matchNum}`;
+  if (compLevel !== "qm") {
+    displayMatch = `${compLevelShortNames[compLevel]} ${setNum}-${matchNum}`;
+  }
+
+  return displayMatch;
+};
