1310813e91f94220fe9942068a027faf65c0c25c
Abhijit Gupta <avgupta456@gmail.com>
2023-03-25 10:48:38 -0400

    Sentry Integration (#248)

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,8 @@
 from dotenv import load_dotenv  # type: ignore
-from fastapi import APIRouter, FastAPI
+from fastapi import APIRouter, FastAPI, Response
 from fastapi.middleware.cors import CORSMiddleware
+
+import sentry_sdk
 
 load_dotenv()
 
@@ -9,10 +11,19 @@ from src.api.router import router as api_router
 from src.constants import CONN_STR, PROD
 from src.data.router import router as data_router
 from src.site.router import router as site_router
+from src.utils.decorators import async_fail_gracefully
 
 """
 SETUP
 """
+
+sentry_sdk.init(
+    dsn="https://d13d19b3a7c54cc79ad19ff65640088c@o4504899523575808.ingest.sentry.io/4504899684270080",
+    # Set traces_sample_rate to 1.0 to capture 100%
+    # of transactions for performance monitoring.
+    # We recommend adjusting this value in production,
+    traces_sample_rate=0.2 if PROD else 1.0,
+)
 
 app = FastAPI(
     title="Statbotics REST API",
@@ -46,6 +57,12 @@ async def read_root():
 @router.get("/info")
 def get_info():
     return {"PROD": PROD, "CONN_STR": "REDACTED" if PROD else CONN_STR}
+
+
+@app.get("/sentry-debug")
+@async_fail_gracefully
+async def trigger_error(response: Response):
+    return 1 / 0
 
 
 app.include_router(router, prefix="", include_in_schema=False)
