b70e803c3cac5682fd27b53e0f596c70fbcfb55e
Abhijit Gupta <avgupta456@gmail.com>
2023-04-05 11:43:25 -0400

    Jump divisions

--- frontend/utils.tsx
+++ frontend/utils.tsx
@@ -1,4 +1,4 @@
-import { PROD, TBA_API_KEY } from "./constants";
+import { PROD, TBA_API_KEY, eventNameMap } from "./constants";
 
 export const classnames = (...args: string[]) => args.join(" ");
 
@@ -58,32 +58,6 @@ export const getMediaUrls = async (teams: number[], year: number) => {
     urls.push(url);
   }
   return urls;
-};
-
-const eventNameMap = {
-  "FIRST in Michigan State Championship presented by DTE Foundation": "FIRST in Michigan - Overall",
-  "FIRST in Michigan State Championship presented by DTE Foundation - DTE ENERGY FOUNDATION Division":
-    "FIRST in Michigan - DTE Energy Foundation Division",
-  "FIRST in Michigan State Championship presented by DTE Foundation - FORD Division":
-    "FIRST in Michigan - Ford Division",
-  "FIRST in Michigan State Championship presented by DTE Foundation - APTIV Division":
-    "FIRST in Michigan - Aptiv Division",
-  "FIRST in Michigan State Championship presented by DTE Foundation - CONSUMERS ENERGY Division":
-    "FIRST in Michigan - Consumers Energy Division",
-  "New England FIRST District Championship": "New England FIRST - Overall",
-  "New England FIRST District Championship - WILSON Division":
-    "New England FIRST - Wilson Division",
-  "New England FIRST District Championship - MEIR Division": "New England FIRST - Meir Division",
-  "FIRST In Texas District Championship presented by Phillips 66": "FIRST In Texas - Overall",
-  "FIRST In Texas District Championship presented by Phillips 66 - APOLLO Division":
-    "FIRST In Texas - Apollo Division",
-  "FIRST In Texas District Championship presented by Phillips 66 - MERCURY Division":
-    "FIRST In Texas - Mercury Division",
-  "FIRST Ontario Provincial Championship": "FIRST Ontario Provincial - Overall",
-  "FIRST Ontario Provincial Championship - SCIENCE Division":
-    "FIRST Ontario Provincial - Science Division",
-  "FIRST Ontario Provincial Championship - TECHNOLOGY Division":
-    "FIRST Ontario Provincial - Technology Division",
 };
 
 export const formatEventName = (eventName: string, limit: number = -1) => {
