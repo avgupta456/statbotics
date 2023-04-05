08dafdbf4f60ad2623cc159a2b76f1662158e007
Abhijit Gupta <avgupta456@gmail.com>
2023-04-05 12:23:24 -0400

    Misc Updates (#267)

--- frontend/constants.tsx
+++ frontend/constants.tsx
@@ -34,3 +34,69 @@ export const RPMapping = {
   2022: ["Cargo RP", "Hanger RP"],
   2023: ["Links RP", "Activation RP"],
 };
+
+export const eventNameMap = {
+  "FIRST in Michigan State Championship presented by DTE Foundation": "FiM Champs - Overall",
+  "FIRST in Michigan State Championship presented by DTE Foundation - DTE ENERGY FOUNDATION Division":
+    "FiM Champs - DTE Energy Foundation Division",
+  "FIRST in Michigan State Championship presented by DTE Foundation - FORD Division":
+    "FiM Champs - Ford Division",
+  "FIRST in Michigan State Championship presented by DTE Foundation - APTIV Division":
+    "FiM Champs - Aptiv Division",
+  "FIRST in Michigan State Championship presented by DTE Foundation - CONSUMERS ENERGY Division":
+    "FiM Champs - Consumers Energy Division",
+  "New England FIRST District Championship": "NE Champs - Overall",
+  "New England FIRST District Championship - WILSON Division": "NE Champs - Wilson Division",
+  "New England FIRST District Championship - MEIR Division": "NE Champs - Meir Division",
+  "FIRST In Texas District Championship presented by Phillips 66": "FIT Champs - Overall",
+  "FIRST In Texas District Championship presented by Phillips 66 - APOLLO Division":
+    "FIT Champs - Apollo Division",
+  "FIRST In Texas District Championship presented by Phillips 66 - MERCURY Division":
+    "FIT Champs - Mercury Division",
+  "FIRST Ontario Provincial Championship": "Ontario Champs - Overall",
+  "FIRST Ontario Provincial Championship - SCIENCE Division": "Ontario Champs - Science Division",
+  "FIRST Ontario Provincial Championship - TECHNOLOGY Division":
+    "Ontario Champs - Technology Division",
+};
+
+export const divisionToMainEvent = {
+  "2023micmp": "2023micmp",
+  "2023micmp1": "2023micmp",
+  "2023micmp2": "2023micmp",
+  "2023micmp3": "2023micmp",
+  "2023micmp4": "2023micmp",
+  "2023necmp": "2023necmp",
+  "2023necmp1": "2023necmp",
+  "2023necmp2": "2023necmp",
+  "2023txcmp": "2023txcmp",
+  "2023txcmp1": "2023txcmp",
+  "2023txcmp2": "2023txcmp",
+  "2023oncmp": "2023oncmp",
+  "2023oncmp1": "2023oncmp",
+  "2023oncmp2": "2023oncmp",
+};
+
+export const mainEventToDivisions = {
+  "2023micmp": [
+    { name: "Overall", key: "2023micmp" },
+    { name: "DTE Energy Foundation", key: "2023micmp1" },
+    { name: "Ford", key: "2023micmp2" },
+    { name: "Aptiv", key: "2023micmp3" },
+    { name: "Consumers Energy", key: "2023micmp4" },
+  ],
+  "2023necmp": [
+    { name: "Overall", key: "2023necmp" },
+    { name: "Meir", key: "2023necmp1" },
+    { name: "Wilson", key: "2023necmp2" },
+  ],
+  "2023txcmp": [
+    { name: "Overall", key: "2023txcmp" },
+    { name: "Apollo", key: "2023txcmp1" },
+    { name: "Mercury", key: "2023txcmp2" },
+  ],
+  "2023oncmp": [
+    { name: "Overall", key: "2023oncmp" },
+    { name: "Technology", key: "2023oncmp1" },
+    { name: "Science", key: "2023oncmp2" },
+  ],
+};
--- frontend/utils.tsx
+++ frontend/utils.tsx
@@ -1,4 +1,4 @@
-import { PROD, TBA_API_KEY } from "./constants";
+import { PROD, TBA_API_KEY, eventNameMap } from "./constants";
 
 export const classnames = (...args: string[]) => args.join(" ");
 
@@ -37,6 +37,7 @@ export const readTBA = async (url: string) => {
 };
 
 export const getMediaUrl = async (team: number, year: number) => {
+  if (team === 0) return null;
   const data = await readTBA(`/team/frc${team}/media/${year}`);
   const image = data.filter((item: any) => item?.preferred)?.[0];
   if (image?.type === "instagram-image") {
@@ -58,32 +59,6 @@ export const getMediaUrls = async (teams: number[], year: number) => {
     urls.push(url);
   }
   return urls;
-};
-
-const eventNameMap = {
-  "FIRST in Michigan State Championship presented by DTE Foundation": "FIRST in Michigan - Overall",
-  "FIRST in Michigan State Championship presented by DTE Foundation - DTE ENERGY FOUNDATION Division":
-    "FIRST in Michigan - DTE Energy Foundation Division",
-  "FIRST in Michigan State Championship presented by DTE Foundation - FORD Division":
-    "FIRST in Michigan - Ford Division",
-  "FIRST in Michigan State Championship presented by DTE Foundation - APTIV Division":
-    "FIRST in Michigan - Aptiv Division",
-  "FIRST in Michigan State Championship presented by DTE Foundation - CONSUMERS ENERGY Division":
-    "FIRST in Michigan - Consumers Energy Division",
-  "New England FIRST District Championship": "New England FIRST - Overall",
-  "New England FIRST District Championship - WILSON Division":
-    "New England FIRST - Wilson Division",
-  "New England FIRST District Championship - MEIR Division": "New England FIRST - Meir Division",
-  "FIRST In Texas District Championship presented by Phillips 66": "FIRST In Texas - Overall",
-  "FIRST In Texas District Championship presented by Phillips 66 - APOLLO Division":
-    "FIRST In Texas - Apollo Division",
-  "FIRST In Texas District Championship presented by Phillips 66 - MERCURY Division":
-    "FIRST In Texas - Mercury Division",
-  "FIRST Ontario Provincial Championship": "FIRST Ontario Provincial - Overall",
-  "FIRST Ontario Provincial Championship - SCIENCE Division":
-    "FIRST Ontario Provincial - Science Division",
-  "FIRST Ontario Provincial Championship - TECHNOLOGY Division":
-    "FIRST Ontario Provincial - Technology Division",
 };
 
 export const formatEventName = (eventName: string, limit: number = -1) => {
