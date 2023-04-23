7d065437df2007a0a7bc1742915d55f609851967
Abhijit Gupta <avgupta456@gmail.com>
2023-04-23 00:11:25 -0400

    End of Season Cleanup (#295)

--- frontend/app/(site)/teams/insightsTable.tsx
+++ frontend/app/(site)/teams/insightsTable.tsx
@@ -144,12 +144,14 @@ const PageTeamInsightsTable = ({
           cell: (info) => formatEPACell(data.year.endgame_stats, info, disableHighlight),
           header: "Endgame EPA",
         }),
+      /*
       year == CURR_YEAR &&
         columnHelper.accessor("next_event_name", {
           cell: (info) =>
             EventLink({ key: info.row.original.next_event_key, event: info.getValue() }),
           header: "Next Event",
         }),
+      */
       columnHelper.accessor("record", {
         cell: (info) => formatCell(info),
         header: "Record",
@@ -211,12 +213,14 @@ const PageTeamInsightsTable = ({
           cell: (info) => formatEPACell(data.year.rp_2_stats, info, disableHighlight),
           header: RPMapping[year][1],
         }),
+      /*
       year == CURR_YEAR &&
         detailedColumnHelper.accessor("next_event_name", {
           cell: (info) =>
             EventLink({ key: info.row.original.next_event_key, event: info.getValue() }),
           header: "Next Event",
         }),
+      */
       detailedColumnHelper.accessor("record", {
         cell: (info) => formatCell(info),
         header: "Record",
