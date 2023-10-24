e518175c29853efa93d429f6931c97f216c9b7b9
Jonah Snider <jonah@jonahsnider.com>
2023-10-23 22:06:45 -0700

    Optimize color fetching logic to deduplicate requests

--- frontend/components/Figures/use-colors.ts
+++ frontend/components/Figures/use-colors.ts
@@ -14,51 +14,59 @@ type ColorData = {
   >;
 };
 
+const requestCache = new Map<string, Promise<Response>>();
+
+async function fetchColors(
+  teams: number[],
+  colors: Map<number, string | undefined> = new Map()
+): Promise<Map<number, string | undefined>> {
+  const colorsUrl = new URL("https://frc-colors.com/api/v1/team");
+
+  if (teams.length > 100) {
+    // Too many teams to fetch in a single request
+    return new Map();
+  }
+
+  if (teams.every((num) => colors.has(num))) {
+    return colors;
+  }
+
+  colorsUrl.search = new URLSearchParams(teams.map((num) => ["team", num.toString()])).toString();
+
+  let json: ColorData = { teams: {} };
+
+  const cacheKey = teams.join(",");
+  const request = requestCache.get(cacheKey) ?? fetch(colorsUrl.toString());
+  requestCache.set(cacheKey, request);
+
+  try {
+    const response = await request;
+
+    if (!response.ok) {
+      throw new Error("Failed to fetch colors");
+    }
+
+    json = await response.json();
+  } catch (error) {
+    console.error("Error fetching colors:", error);
+    return colors;
+  }
+
+  return new Map(
+    Object.values(json.teams).map(
+      (teamColors) => [teamColors.teamNumber, teamColors.colors?.primaryHex ?? undefined] as const
+    )
+  );
+}
+
 export function useColors(teams: number[]): Map<number, string | undefined> {
   /** Maps team num to hex color code */
   const [colors, setColors] = useState<Map<number, string | undefined>>(new Map());
 
   useEffect(() => {
-    const fetchColors = async () => {
-      const colorsUrl = new URL("https://frc-colors.com/api/v1/team");
-
-      if (teams.length > 100) {
-        setColors(new Map());
-        return;
-      }
-
-      if (teams.every((num) => colors.has(num))) {
-        return;
-      }
-
-      colorsUrl.search = new URLSearchParams(
-        teams.map((num) => ["team", num.toString()])
-      ).toString();
-
-      let json: ColorData = { teams: {} };
-
-      try {
-        const response = await fetch(colorsUrl.toString());
-
-        if (!response.ok) {
-          throw new Error("Failed to fetch colors");
-        }
-
-        json = await response.json();
-      } catch (error) {
-        console.error("Error fetching colors:", error);
-      }
-
-      const newColors = new Map<number, string>();
-
-      for (const teamColors of Object.values(json.teams)) {
-        newColors.set(teamColors.teamNumber, teamColors.colors?.primaryHex ?? undefined);
-      }
-
+    fetchColors(teams, colors).then((newColors) => {
       setColors(newColors);
-    };
-
-    fetchColors();
+    });
   }, [teams]);
 
   return colors;
