c638a339df7e21aa5656131a008c92425c2d3353
Jonah Snider <jonah@jonahsnider.com>
2023-11-23 13:27:27 -0800

    FRC Colors Edge Cases (#347)

--- frontend/components/Figures/colors.tsx
+++ frontend/components/Figures/colors.tsx
@@ -1,5 +1,9 @@
 import { PropsWithChildren, createContext, useContext, useEffect, useMemo, useState } from "react";
 
+function colorLuminance(r: number, g: number, b: number) {
+  return r * 0.299 + g * 0.587 + b * 0.114;
+}
+
 type TeamColors = Map<number, string | undefined>;
 
 type IColorsContext = {
@@ -31,13 +35,21 @@ type ColorData = {
 };
 
 async function fetchColors(teams: number[]): Promise<TeamColors | undefined> {
-  if (teams.length > 100) {
+  const uniqueTeams = new Set(
+    // Convert offseason robots represented with really large numbers to their main team number
+    // ex. team 1234B as 123400001
+    teams.map((num) => num.toString().replace(/(\d+)0000\d+$/, "$1"))
+  );
+
+  if (uniqueTeams.size > 100) {
     return undefined;
   }
 
   const colorsUrl = new URL("https://frc-colors.com/api/v1/team");
 
-  colorsUrl.search = new URLSearchParams(teams.map((num) => ["team", num.toString()])).toString();
+  colorsUrl.search = new URLSearchParams(
+    Array.from(uniqueTeams).map((num) => ["team", num.toString()])
+  ).toString();
 
   const request = fetch(colorsUrl.toString());
 
@@ -70,7 +82,28 @@ export const ColorsProvider = ({ children, teams }: Props) => {
   useEffect(() => {
     setIsLoading(true);
     fetchColors(teams).then((newColors) => {
-      setColors(newColors);
+      const filteredColors: TeamColors = new Map();
+
+      for (const [team, color] of Array.from(newColors)) {
+        if (!color) {
+          filteredColors.set(team, undefined);
+          continue;
+        }
+
+        const r = parseInt(color.slice(1, 3), 16);
+        const g = parseInt(color.slice(3, 5), 16);
+        const b = parseInt(color.slice(5, 7), 16);
+
+        const luminance = colorLuminance(r, g, b);
+
+        if (luminance > 0.9 * 255) {
+          continue;
+        }
+
+        filteredColors.set(team, color);
+      }
+
+      setColors(filteredColors);
       setIsLoading(false);
     });
   }, [teams]);
