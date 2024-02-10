bd91d209b94dd6bb6f8028fb6f116ceee14003a5
Abhijit Gupta <avgupta456@gmail.com>
2024-02-10 14:27:33 -0500

    Add footer

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,5 +1,7 @@
 import React from "react";
 
+import Image from "next/image";
+import Link from "next/link";
 import Script from "next/script";
 
 import "./globals.css";
@@ -45,6 +47,50 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
         <div className="min-h-screen flex flex-col bg-white text-gray-800">
           <Navbar />
           {children}
+          <footer className="w-full border-t border-gray-300 text-gray-600 text-center pt-3 pb-2">
+            <p className="h-4 sm:h-5 lg:h-6 flex justify-center items-center gap-1 text-xs sm:text-sm lg:text-base">
+              Powered by{" "}
+              <Link
+                href="https://www.thebluealliance.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-blue-500 hover:underline"
+              >
+                The Blue Alliance
+              </Link>
+              <Link
+                href="https://www.thebluealliance.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="relative h-4 sm:h-5 lg:h-6 w-4 sm:h-5 lg:w-6 mx-1"
+              >
+                <Image src="/tba.png" alt="TBA" unoptimized fill className="object-contain" />
+              </Link>{" "}
+              | Sponsored by{" "}
+              <Link
+                href="https://www.thethriftybot.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="text-blue-500 hover:underline"
+              >
+                The Thrifty Bot
+              </Link>
+              <Link
+                href="https://www.thethriftybot.com/"
+                target="_blank"
+                rel="noopener noreferrer"
+                className="relative h-4 sm:h-5 lg:h-6 w-4 sm:w-5 lg:w-6 ml-1"
+              >
+                <Image
+                  src="/thrifty.png"
+                  alt="Thrifty"
+                  unoptimized
+                  fill
+                  className="object-contain"
+                />
+              </Link>
+            </p>
+          </footer>
         </div>
       </body>
     </html>
