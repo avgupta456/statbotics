5ea25914ccb89d3e4b863554a79cb631ac8a2910
Abhijit Gupta <avgupta456@gmail.com>
2024-02-26 22:59:34 -0500

    Hardcode constants

--- backend/src/data/avg.py
+++ backend/src/data/avg.py
@@ -27,34 +27,34 @@ def process_year(year: Year, matches: List[Match]) -> Year:
         sd = statistics.stdev(clean_arr)
         return r(mean, 2), r(sd, 2)
 
-    N = 2 * len(week_one_matches)
-    if year.year == CURR_YEAR and N < 100:
-        year.score_mean, year.score_sd = 39.74, 24.11
-        year.no_foul_mean = 37.6
-        year.foul_mean = 2.15
-        year.auto_mean = 16.05
-        year.teleop_mean = 18.77
-        year.endgame_mean = 2.77
-        year.rp_1_mean = 0.06
+    # TODO: Delete after Week 1
+    if year.year == CURR_YEAR:
+        year.score_mean, year.score_sd = 50.59, 23.01
+        year.no_foul_mean = 44.35
+        year.foul_mean = 6.24
+        year.auto_mean = 16.7
+        year.teleop_mean = 23.69
+        year.endgame_mean = 3.97
+        year.rp_1_mean = 0.25
         year.rp_2_mean = 0.01
-        year.tiebreaker_mean = 0.1
-        year.comp_1_mean = 2.65
-        year.comp_2_mean = 2.76
-        year.comp_3_mean = 13.4
-        year.comp_4_mean = 9.19
-        year.comp_5_mean = 18.77
-        year.comp_6_mean = 2.6
-        year.comp_7_mean = 2.73
-        year.comp_8_mean = 9.35
-        year.comp_9_mean = 29.45
-        year.comp_10_mean = 0.95
-        year.comp_11_mean = 11.95
-        year.comp_12_mean = 32.18
-        year.comp_13_mean = 1.4
-        year.comp_14_mean = 1.35
-        year.comp_15_mean = 0.01
-        year.comp_16_mean = 0.01
-        year.comp_17_mean = 0.02
+        year.tiebreaker_mean = 0.26
+        year.comp_1_mean = 3.64
+        year.comp_2_mean = 2.62
+        year.comp_3_mean = 13.06
+        year.comp_4_mean = 11.21
+        year.comp_5_mean = 23.69
+        year.comp_6_mean = 3.76
+        year.comp_7_mean = 3.77
+        year.comp_8_mean = 10.06
+        year.comp_9_mean = 32.97
+        year.comp_10_mean = 1.67
+        year.comp_11_mean = 13.83
+        year.comp_12_mean = 36.74
+        year.comp_13_mean = 1.39
+        year.comp_14_mean = 1.75
+        year.comp_15_mean = 0.05
+        year.comp_16_mean = 0.71
+        year.comp_17_mean = 0.07
         return year
 
     year.score_mean, year.score_sd = get_mean_sd(
