3edfa034f7c93361afc2c8ae10c39090641f2c59
Abhijit Gupta <avgupta456@gmail.com>
2024-03-19 08:07:34 -0400

    Update shared.tsx

--- frontend/app/(site)/events/shared.tsx
+++ frontend/app/(site)/events/shared.tsx
@@ -61,7 +61,7 @@ const EventsLayout = ({
   const today = new Date().toISOString().split("T")[0];
 
   const ongoingEvents = sortedData
-    ?.filter((event) => event.status === "Ongoing" || event.week >= CURR_WEEK)
+    ?.filter((event) => event.status === "Ongoing" && event.week >= CURR_WEEK)
     .sort((a, b) => {
       if (a.current_match > 0 && b.current_match === 0) return -1;
       if (a.current_match === 0 && b.current_match > 0) return 1;
