239c728c26bc0fa1dd42c32c21cb42d3b4c14030
Abhijit Gupta <avgupta456@gmail.com>
2025-02-27 22:11:21 -0500

    Update teams page to client side

--- frontend/src/app/(site)/teams/page.tsx
+++ frontend/src/app/(site)/teams/page.tsx
@@ -1,23 +1,80 @@
-import React from "react";
+"use client";
+
+import React, { useCallback, useContext, useEffect, useState } from "react";
+
+import { useSearchParams } from "next/navigation";
 
 import { validateFilters } from "../../../components/filter";
-import PageContent from "./main";
+import { CURR_YEAR } from "../../../constants";
+import { getYearTeamYears } from "../../../server/teams";
+import { TeamYearsData } from "../../../types/data";
+import { AppContext } from "../context";
+import PageLayout from "../shared/layout";
+import Tabs from "./tabs";
 
-// cache this page for 5 minutes
-export const revalidate = 60 * 5;
+const Page = () => {
+  const { teamYearDataDict, setTeamYearDataDict, year, setYear } = useContext(AppContext);
+  const data: TeamYearsData | undefined = teamYearDataDict[year];
+  const [error, setError] = useState(false);
 
-export const metadata = {
-  title: "Teams - Statbotics",
-};
+  const searchParams = useSearchParams();
 
-const Page = ({ searchParams: { year, country, state, district } }) => {
-  const paramFilters = validateFilters(
-    { year, country, state, district },
-    ["year", "country", "state", "district"],
-    [undefined, "", "", ""]
+  useEffect(() => {
+    document.title = "Teams - Statbotics";
+  }, []);
+
+  const getFilters = useCallback(() => {
+    const filters = {
+      year: searchParams.get("year"),
+      country: searchParams.get("country"),
+      state: searchParams.get("state"),
+      district: searchParams.get("district"),
+      is_competing: year === CURR_YEAR ? "" : undefined,
+    };
+
+    return validateFilters(
+      filters,
+      ["year", "country", "state", "district"],
+      [undefined, "", "", ""]
+    );
+  }, [searchParams, year]);
+
+  const [filters, setFilters] = useState(getFilters);
+
+  useEffect(() => {
+    const filterYear = parseInt(searchParams.get("year") || year.toString());
+    if (filterYear !== year) setYear(filterYear);
+
+    setFilters(getFilters());
+  }, [searchParams, year, setYear, getFilters]);
+
+  useEffect(() => {
+    setError(false);
+  }, [year]);
+
+  useEffect(() => {
+    const getDataForYear = async (year: number) => {
+      if (teamYearDataDict[year] || error) {
+        return;
+      }
+
+      const data: TeamYearsData = await getYearTeamYears(year);
+
+      if (!data) {
+        setError(true);
+      } else {
+        setTeamYearDataDict((prev) => ({ ...prev, [year]: data }));
+      }
+    };
+
+    getDataForYear(year);
+  }, [teamYearDataDict, setTeamYearDataDict, year, error]);
+
+  return (
+    <PageLayout title="Teams" year={year} setYear={setYear}>
+      <Tabs year={year} data={data} error={error} filters={filters} setFilters={setFilters} />
+    </PageLayout>
   );
-
-  return <PageContent paramFilters={paramFilters} />;
 };
 
 export default Page;
