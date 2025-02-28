e678d1ebb23a8618a755ee82a54eb177f9818c32
Abhijit Gupta <avgupta456@gmail.com>
2025-02-27 22:12:08 -0500

    Update team page

--- frontend/src/app/(site)/team/[team]/page.tsx
+++ frontend/src/app/(site)/team/[team]/page.tsx
@@ -2,21 +2,20 @@
 
 import React, { useEffect } from "react";
 
+import { useParams } from "next/navigation";
+
 import { CURR_YEAR } from "../../../../constants";
 import PageContent from "./main";
 
-// do not cache this page
-export const revalidate = 0;
-
-const Page = ({ params }: { params: { team: number } }) => {
-  const { team } = params;
+const Page = () => {
+  const { team } = useParams();
   const paramYear = CURR_YEAR;
 
   useEffect(() => {
     document.title = `Team ${team} - Statbotics`;
   }, [team]);
 
-  return <PageContent team={team} paramYear={paramYear} />;
+  return <PageContent team={Number(team)} paramYear={paramYear} />;
 };
 
 export default Page;
