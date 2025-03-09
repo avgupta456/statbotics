a44cc326dd976492ba7480e90f21a7207bf7cfcd
Abhijit Gupta <avgupta456@gmail.com>
2025-03-09 00:16:28 -0500

    Remove CRDB_CLUSTER

--- backend/src/constants.py
+++ backend/src/constants.py
@@ -13,7 +13,6 @@ BACKEND_URL = "https://api.statbotics.io" if PROD else "http://localhost:8001"
 CRDB_USER = os.getenv("CRDB_USER", "")
 CRDB_PWD = os.getenv("CRDB_PWD", "")
 CRDB_HOST = os.getenv("CRDB_HOST", "")
-CRDB_CLUSTER = os.getenv("CRDB_CLUSTER", "")
 
 CONN_STR = (
     (
@@ -23,16 +22,17 @@ CONN_STR = (
         + CRDB_PWD
         + "@"
         + CRDB_HOST
-        + "/statbotics3?sslmode=verify-full&sslrootcert=root.crt&options=--cluster%3D"
-        + CRDB_CLUSTER
+        + "/statbotics3?sslmode=verify-full&sslrootcert=root.crt"
     )
     if PROD
     else "cockroachdb://root@localhost:26257/statbotics3?sslmode=disable"
 )
 
-ASYNC_CONN_STR = CONN_STR.replace("cockroachdb://", "cockroachdb+asyncpg://").split(
-    "?sslmode"
-)[0]
+ASYNC_CONN_STR = (
+    CONN_STR.replace("cockroachdb://", "cockroachdb+asyncpg://")
+    .replace("?sslmode=disable", "")
+    .replace("?sslmode=verify-full&sslrootcert=root.crt", "")
+)
 
 # API
 
